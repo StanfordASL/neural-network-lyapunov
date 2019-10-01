0458fa9bbb8575e702ab1f29f0a640d0e80e4a3a
Hongkai Dai <daihongkai@gmail.com>
2019-10-01 12:20:10 -0700

    Add switch linear system. (#6)

---
+++ HybridLinearSystem.py
@@ -0,0 +1,155 @@
+import torch
+import numpy as np
+
+from utils import check_shape_and_type, replace_binary_continuous_product
+
+
+class HybridLinearSystem:
+    """
+    This system models the hybrid linear system
+    x[n+1] = Aᵢ*x[n] + Bᵢ*u[n] + cᵢ
+    if Pᵢ * [x[n]; u[n]] <= qᵢ
+    i = 1, ..., K.
+    Namely there are K different modes, each mode constrains the state/control
+    of the dynamical system to be within a polytope. Inside each mode, the
+    discrete time dynamics is affine.
+    Note that the polytope Pᵢ * [x[n]; u[n]] <= qᵢ has to be bounded.
+    """
+
+    def __init__(self, x_dim, u_dim, dtype):
+        """
+        @param x_dim The dimension of x.
+        @param u_dim The dimension of u.
+        @param dtype The torch datatype of A, B, c, P, q.
+        """
+        self.dtype = dtype
+
+        self.x_dim = x_dim
+        self.u_dim = u_dim
+        self.A = []
+        self.B = []
+        self.c = []
+        self.P = []
+        self.q = []
+        self.num_modes = 0
+
+    def add_mode(self, Ai, Bi, ci, Pi, qi):
+        """
+        Add a new mode
+        x[n+1] = Aᵢ*x[n] + Bᵢ*u[n] + cᵢ
+        if Pᵢ * [x[n]; u[n]] <= qᵢ
+        @param Ai A x_dim * x_dim torch matrix.
+        @param Bi A x_dim * u_dim torch matrix.
+        @param ci A x_dim * 1 torch column vector.
+        @param Pi A num_constraint * (x_dim + u_dim) torch matrix.
+        @param qi A num_constraint * 1 torch column vector.
+        @note that the polytope Pᵢ * [x[n]; u[n]] <= qᵢ has to be bounded.
+        """
+        check_shape_and_type(Ai, (self.x_dim, self.x_dim), self.dtype)
+        check_shape_and_type(Bi, (self.x_dim, self.u_dim), self.dtype)
+        check_shape_and_type(ci, (self.x_dim, 1), self.dtype)
+        num_constraint = Pi.shape[0]
+        check_shape_and_type(Pi, (num_constraint, self.x_dim + self.u_dim),
+                             self.dtype)
+        check_shape_and_type(qi, (num_constraint, 1), self.dtype)
+        self.A.append(Ai)
+        self.B.append(Bi)
+        self.c.append(ci)
+        self.P.append(Pi)
+        self.q.append(qi)
+        self.num_modes += 1
+
+    def mixed_integer_constraints(self, x_lo, x_up, u_lo, u_up):
+        """
+        We can rewrite the hybrid dynamics as mixed integer linear constraints.
+        We denote αᵢ = 1 if the system is in mode i.
+        x[n+1] = ∑ᵢαᵢAᵢx[n] + αᵢBᵢu[n]+αᵢcᵢ (1)
+        Pᵢ * αᵢ*[x[n];u[n]] ≤ qᵢαᵢ          (2)
+        Note that there we still have the product αᵢ*x[n], αᵢ*u[n]. To get
+        rid of the product, we introduce slack variable s[n], t[n], defined
+        as sᵢ[n] = αᵢ*x[n], tᵢ[n] = αᵢ*u[n]. The condition sᵢ[n] = αᵢ*x[n],
+        tᵢ[n] = αᵢ*u[n] can be enforced through linear constraints, @see
+        replace_binary_continuous_product() function.
+        The condition (1) and (2) can be written as the mixed-integer linear
+        constraints
+        x[n+1] = Aeq_slack * slack + Aeq_alpha * α
+        Ain_x*x[n] + Ain_u*u[n] + Ain_slack * slack + Ain_alpha*α ≤ rhs_in
+        where slack = [s₁[n];s₂[n];...s_K[n];t₁[n];t₂[n];...;t_K[n]]
+        α = [α₁;α₂;...;α_K].
+        @param x_lo The lower bound of x[n], a column vector.
+        @param x_up The upper bound of x[n], a column vector
+        @param u_lo The lower bound of u[n], a column vector.
+        @param u_up The upper bound of u[n], a column vector
+        @return (Aeq_slack, Aeq_alpha, Ain_x, Ain_u, Ain_slack, Ain_alpha,
+        rhs_in)
+        @note 1. This function doesn't require the polytope
+                 Pᵢ * [x[n]; u[n]] <= qᵢ to be mutually exclusive.
+              2. We do not impose the constraint that one and only one mode
+                 is active. The user should impose this constraint separately.
+        """
+        check_shape_and_type(x_lo, (self.x_dim, 1), self.dtype)
+        check_shape_and_type(x_up, (self.x_dim, 1), self.dtype)
+        check_shape_and_type(u_up, (self.u_dim, 1), self.dtype)
+        check_shape_and_type(u_lo, (self.u_dim, 1), self.dtype)
+        assert(torch.all(x_lo <= x_up))
+        assert(torch.all(u_lo <= u_up))
+        Aeq_slack = torch.cat((torch.cat(self.A, dim=1),
+                               torch.cat(self.B, dim=1)), dim=1)
+        Aeq_alpha = torch.cat(self.c, dim=1)
+
+        num_slack = (self.x_dim + self.u_dim) * self.num_modes
+        num_ineq = np.sum(np.array([Pi.shape[0]
+                                    for Pi in self.P])) + num_slack * 4
+        Ain_x = torch.zeros(num_ineq, self.x_dim, dtype=self.dtype)
+        Ain_u = torch.zeros(num_ineq, self.u_dim, dtype=self.dtype)
+        Ain_slack = torch.zeros(num_ineq, num_slack, dtype=self.dtype)
+        Ain_alpha = torch.zeros(num_ineq, self.num_modes, dtype=self.dtype)
+        rhs_in = torch.zeros(num_ineq, 1, dtype=self.dtype)
+
+        ineq_count = 0
+
+        # We first add the constraint sᵢ[n] = αᵢ*x[n], tᵢ[n] = αᵢ*u[n]
+
+        def s_index(i, j):
+            # The index of sᵢ[n][j, 0] in the slack variable.
+            return i * self.x_dim + j
+
+        def t_index(i, j):
+            # The index of tᵢ[n][j, 0] in the slack variable.
+            return self.num_modes * self.x_dim + i * self.u_dim + j
+        for i in range(self.num_modes):
+            for j in range(self.x_dim):
+                (Ain_x[ineq_count: ineq_count + 4, j:j+1],
+                 Ain_slack[ineq_count: ineq_count +
+                           4, s_index(i, j):s_index(i, j) + 1],
+                 Ain_alpha[ineq_count:ineq_count + 4, i:i+1],
+                 rhs_in[ineq_count:ineq_count + 4, 0:1]) =\
+                    replace_binary_continuous_product(x_lo[j][0], x_up[j][0],
+                                                      self.dtype)
+                ineq_count += 4
+            for j in range(self.u_dim):
+                (Ain_u[ineq_count: ineq_count + 4, j:j+1],
+                 Ain_slack[ineq_count:ineq_count+4,
+                           t_index(i, j):t_index(i, j)+1],
+                 Ain_alpha[ineq_count:ineq_count+4, i:i+1],
+                 rhs_in[ineq_count:ineq_count+4, 0:1]) =\
+                    replace_binary_continuous_product(u_lo[j][0], u_up[j][0],
+                                                      self.dtype)
+                ineq_count += 4
+
+        # Add the constraint Pᵢ * αᵢ*[x[n];u[n]] ≤ qᵢαᵢ
+        # Namely Pᵢ * slack ≤ qᵢαᵢ
+        for i in range(self.num_modes):
+            Ain_slack[ineq_count: ineq_count+self.P[i].shape[0],
+                      i*self.x_dim: (i+1) * self.x_dim] =\
+                self.P[i][:, :self.x_dim].clone()
+            Ain_slack[ineq_count:ineq_count+self.P[i].shape[0],
+                      self.num_modes * self.x_dim + i * self.u_dim:
+                      self.num_modes * self.x_dim + (i+1) * self.u_dim] =\
+                self.P[i][:, self.x_dim:self.x_dim + self.u_dim].clone()
+            Ain_alpha[ineq_count: ineq_count +
+                      self.P[i].shape[0], i:i+1] = -self.q[i]
+            ineq_count += self.P[i].shape[0]
+
+        return (Aeq_slack, Aeq_alpha, Ain_x, Ain_u, Ain_slack, Ain_alpha,
+                rhs_in)
---
+++ test/HybridLinearSystemTest.py
@@ -0,0 +1,128 @@
+import sys
+sys.path.append("..")
+import torch
+import unittest
+import HybridLinearSystem
+
+
+class HybridLinearSystemTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_constructor(self):
+        dut = HybridLinearSystem.HybridLinearSystem(3, 2, torch.float64)
+        self.assertEqual(dut.x_dim, 3)
+        self.assertEqual(dut.u_dim, 2)
+        self.assertEqual(dut.dtype, torch.float64)
+        self.assertEqual(dut.num_modes, 0)
+
+    def test_add_mode(self):
+        dut = HybridLinearSystem.HybridLinearSystem(2, 1, torch.float64)
+        A0 = torch.tensor([[1, 2], [2, 1]], dtype=dut.dtype)
+        B0 = torch.tensor([[2], [3]], dtype=dut.dtype)
+        c0 = torch.tensor([[-1], [2]], dtype=dut.dtype)
+        P0 = torch.cat((torch.eye(3, dtype=dut.dtype),
+                        -torch.eye(3, dtype=dut.dtype)), dim=0)
+        q0 = torch.tensor([[1], [2], [3], [1], [2], [3]], dtype=dut.dtype)
+        dut.add_mode(A0, B0, c0, P0, q0)
+        self.assertEqual(dut.num_modes, 1)
+
+    def test_mixed_integer_constraints(self):
+        dut = HybridLinearSystem.HybridLinearSystem(2, 1, torch.float64)
+        A0 = torch.tensor([[1, 2], [2, 1]], dtype=dut.dtype)
+        B0 = torch.tensor([[2], [3]], dtype=dut.dtype)
+        c0 = torch.tensor([[-1], [2]], dtype=dut.dtype)
+        P0 = torch.cat((torch.eye(3, dtype=dut.dtype),
+                        -torch.eye(3, dtype=dut.dtype)), dim=0)
+        q0 = torch.tensor([[1], [2], [3], [1], [2], [3]], dtype=dut.dtype)
+        dut.add_mode(A0, B0, c0, P0, q0)
+        A1 = torch.tensor([[3, 2], [-2, 1]], dtype=dut.dtype)
+        B1 = torch.tensor([[-2], [4]], dtype=dut.dtype)
+        c1 = torch.tensor([[3], [-2]], dtype=dut.dtype)
+        P1 = torch.cat((3 * torch.eye(3, dtype=dut.dtype),
+                        -2 * torch.eye(3, dtype=dut.dtype),
+                        torch.tensor([[1, 2, 3]], dtype=dut.dtype)), dim=0)
+        q1 = torch.tensor([[12], [2], [4], [-1], [1], [3], [7]],
+                          dtype=dut.dtype)
+        dut.add_mode(A1, B1, c1, P1, q1)
+        A2 = torch.tensor([[3, -2], [6, 1]], dtype=dut.dtype)
+        B2 = torch.tensor([[2], [7]], dtype=dut.dtype)
+        c2 = torch.tensor([[1], [-4]], dtype=dut.dtype)
+        P2 = torch.cat((2 * torch.eye(3, dtype=dut.dtype),
+                        -5 * torch.eye(3, dtype=dut.dtype),
+                        torch.tensor([[4, 2, 1]], dtype=dut.dtype)), dim=0)
+        q2 = torch.tensor([[1], [3], [3], [-1], [1], [3], [4]],
+                          dtype=dut.dtype)
+        dut.add_mode(A2, B2, c2, P2, q2)
+
+        x_lo = torch.tensor([[-3], [-2]], dtype=dut.dtype)
+        x_up = torch.tensor([[10], [8]], dtype=dut.dtype)
+        u_lo = torch.tensor([[-5]], dtype=dut.dtype)
+        u_up = torch.tensor([[10]], dtype=dut.dtype)
+        (Aeq_slack, Aeq_alpha, Ain_x, Ain_u, Ain_slack, Ain_alpha, rhs_in) =\
+            dut.mixed_integer_constraints(x_lo, x_up, u_lo, u_up)
+
+        def generate_xu(mode, expect_in_mode):
+            # @param expect_in_mode. Do you want to generate x/u in that mode?
+            is_in_mode = not expect_in_mode
+            while is_in_mode != expect_in_mode:
+                x = torch.empty((2, 1), dtype=dut.dtype)
+                u = torch.empty((1, 1), dtype=dut.dtype)
+                for i in range(dut.x_dim):
+                    x[i] = torch.DoubleTensor(1, 1).\
+                        uniform_(x_lo[i, 0], x_up[i, 0])[0, 0]
+                for i in range(dut.u_dim):
+                    u[i] = torch.DoubleTensor(1, 1).\
+                        uniform_(u_lo[i, 0], u_up[i, 0])[0, 0]
+                if torch.all(dut.P[mode] @ torch.cat((x, u), dim=0) <=
+                             dut.q[mode]):
+                    is_in_mode = True
+                else:
+                    is_in_mode = False
+            return (x, u)
+
+        def test_mode(mode):
+            (x, u) = generate_xu(mode, True)
+            # First find x and u in this mode.
+            x_next = dut.A[mode] @ x + dut.B[mode] @ u + dut.c[mode]
+
+            alpha = torch.zeros(dut.num_modes, 1, dtype=dut.dtype)
+            alpha[mode][0] = 1
+            s = torch.zeros(dut.num_modes * dut.x_dim, 1, dtype=dut.dtype)
+            t = torch.zeros(dut.num_modes * dut.u_dim, 1, dtype=dut.dtype)
+            s[dut.x_dim * mode: dut.x_dim * (mode + 1)] = x
+            t[dut.u_dim * mode: dut.u_dim * (mode + 1)] = u
+            slack = torch.cat((s, t), dim=0)
+            self.assertTrue(
+                torch.all(torch.abs(x_next - (Aeq_slack @ slack
+                                              + Aeq_alpha @ alpha)) < 1E-12))
+            lhs_in = Ain_x @ x + Ain_u @ u + Ain_slack @ slack\
+                + Ain_alpha @ alpha
+            self.assertTrue(torch.all(lhs_in <= rhs_in + 1E-12))
+
+        for mode in range(dut.num_modes):
+            test_mode(mode)
+            test_mode(mode)
+            test_mode(mode)
+
+        def test_ineq(mode):
+            # Randomly sample x and u. If x and u are not in that mode, then
+            # there should be no slack variables such that the inequality
+            # constraints are satisfied.
+            (x, u) = generate_xu(mode, False)
+            alpha = torch.zeros(dut.num_modes, 1, dtype=dut.dtype)
+            alpha[mode] = 1
+            s = torch.zeros(dut.num_modes * dut.x_dim, 1, dtype=dut.dtype)
+            t = torch.zeros(dut.num_modes * dut.u_dim, 1, dtype=dut.dtype)
+            s[dut.x_dim * mode: dut.x_dim * (mode + 1)] = x
+            t[dut.u_dim * mode: dut.u_dim * (mode + 1)] = u
+            slack = torch.cat((s, t), dim=0)
+            lhs = Ain_x @ x + Ain_u @ u + Ain_slack @ slack + Ain_alpha @ alpha
+            self.assertFalse(torch.all(lhs < rhs_in + 1E-12))
+
+        for mode in range(dut.num_modes):
+            test_ineq(mode)
+
+
+if __name__ == "__main__":
+    unittest.main()
