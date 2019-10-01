c723c5ed618f0a12ca19cee37fa121110e89e211
Benoit Landry <ben.acc.landry@gmail.com>
2019-10-01 13:03:53 -0700

    working upper bound problem

--- ModelBoundsTest.py
+++ ModelBoundsTest.py
@@ -36,19 +36,19 @@ class ModelBoundsUpperBound(unittest.TestCase):
     def test_upper_bound(self):
         sys = BallPaddleSystem.BallPaddleSystem(dt=.01)
     
-        Q = torch.ones(3,3,dtype=sys.dtype)*0.1*0.
-        q = torch.ones(3,dtype=sys.dtype)*0.1*0.
-        R = torch.ones(1,1,dtype=sys.dtype)*2.*0.
-        r = torch.ones(1,dtype=sys.dtype)*0.1*0.
-        Z = torch.ones(1,1,dtype=sys.dtype)*0.*0.
-        z = torch.ones(1,dtype=sys.dtype)*0.1*0.
+        Q = torch.ones(3,3,dtype=sys.dtype)*0.1
+        q = torch.ones(3,dtype=sys.dtype)*0.1
+        R = torch.ones(1,1,dtype=sys.dtype)*2.
+        r = torch.ones(1,dtype=sys.dtype)*0.1
+        Z = torch.ones(1,1,dtype=sys.dtype)*0.
+        z = torch.ones(1,dtype=sys.dtype)*0.1
     
-        Qt = torch.ones(3,3,dtype=sys.dtype)*0.1*0.
-        qt = torch.ones(3,dtype=sys.dtype)*0.1*0.
-        Rt = torch.ones(1,1,dtype=sys.dtype)*2.*0.
-        rt = torch.ones(1,dtype=sys.dtype)*0.1*0.
-        Zt = torch.ones(1,1,dtype=sys.dtype)*0.*0.
-        zt = torch.ones(1,dtype=sys.dtype)*0.1*0.
+        Qt = torch.ones(3,3,dtype=sys.dtype)*0.1
+        qt = torch.ones(3,dtype=sys.dtype)*0.1
+        Rt = torch.ones(1,1,dtype=sys.dtype)*2.
+        rt = torch.ones(1,dtype=sys.dtype)*0.1
+        Zt = torch.ones(1,1,dtype=sys.dtype)*0.
+        zt = torch.ones(1,dtype=sys.dtype)*0.1
     
         x_lo_traj = torch.ones(3,dtype=sys.dtype)*-100.
         x_up_traj = torch.ones(3,dtype=sys.dtype)*100.
@@ -83,7 +83,6 @@ class ModelBoundsUpperBound(unittest.TestCase):
         y = cp.Variable(num_y)
         gamma = cp.Variable(num_gamma, boolean=True)
         obj = cp.Minimize(.5*cp.quad_form(y,Q1) + .5*cp.quad_form(gamma,Q2) + q1@y + q2@gamma + k)
-        # obj = cp.Minimize(0.)
         con = [
             A1@y + A2@gamma == b,
             G1@y + G2@gamma <= h,
@@ -91,76 +90,60 @@ class ModelBoundsUpperBound(unittest.TestCase):
         prob = cp.Problem(obj,con)
         prob.solve(solver=cp.GUROBI, verbose=False)
         epsilon = obj.value
-        # epsilon = q1[N*4:]@y.value[N*4:] + q2[N:]@gamma.value[N:] + k
-        print(epsilon)
-        
-        x0 = torch.Tensor(y.value[:3]).type(sys.dtype)
-    
-        vf = ValueToOptimization.ValueFunction(sys)
-        (Q2_traj, Q3_traj, q2_traj, q3_traj,
-        Ain1_traj, Ain2_traj, Ain3_traj, brhs_in_traj, 
-        Aeq1_traj, Aeq2_traj, Aeq3_traj, brhs_eq_traj) = vf.traj_opt_x0xN(
-            Q, R, Z, q, r, z, 
-            Qt, Rt, Zt, qt, rt, zt, 
-            N, xN,
-            x_lo_traj, x_up_traj, u_lo, u_up,
-            x0=x0)        
-        Q2_traj = Q2_traj.detach().numpy()
-        q2_traj = q2_traj.detach().numpy()
-        Q3_traj = Q3_traj.detach().numpy()
-        q3_traj = q3_traj.detach().numpy()
-        Ain1_traj = Ain1_traj.detach().numpy()
-        Ain2_traj = Ain2_traj.detach().numpy()
-        Ain3_traj = Ain3_traj.detach().numpy()
-        brhs_in_traj = brhs_in_traj.detach().numpy()    
-        Aeq1_traj = Aeq1_traj.detach().numpy()
-        Aeq2_traj = Aeq2_traj.detach().numpy()
-        Aeq3_traj = Aeq3_traj.detach().numpy()
-        brhs_eq_traj = brhs_eq_traj.detach().numpy()
-        x = cp.Variable(Ain1_traj.shape[1])
-        s = cp.Variable(Ain2_traj.shape[1])
-        alpha = cp.Variable(Ain3_traj.shape[1],boolean=True)
-        obj = cp.Minimize(.5*cp.quad_form(s,Q2_traj) + q2_traj*s + .5*cp.quad_form(alpha,Q3_traj) + q3_traj*alpha)
-        con = [
-            Ain1_traj@x + Ain2_traj@s + Ain3_traj@alpha <= brhs_in_traj.squeeze(),
-            Aeq1_traj@x + Aeq2_traj@s + Aeq3_traj@alpha == brhs_eq_traj.squeeze()
-        ]
-        prob = cp.Problem(obj,con)
-        prob.solve(solver=cp.GUROBI, verbose=False)
-        value = obj.value
-        print(value)
-        
-        relu_opt = ReLUToOptimization.ReLUFreePattern(self.model, sys.dtype)
-        Ain1_nn, Ain2_nn, Ain3_nn, rhs_in_nn, Aeq1_nn, Aeq2_nn, Aeq3_nn, rhs_eq_nn, a_out_nn, b_out_nn, z_lo_nn, z_up_nn = relu_opt.output_constraint(self.model, x_lo, x_up)
-        Ain1_nn = Ain1_nn.detach().numpy()
-        Ain2_nn = Ain2_nn.detach().numpy()
-        Ain3_nn = Ain3_nn.detach().numpy()
-        rhs_in_nn = rhs_in_nn.squeeze().detach().numpy()
-        Aeq1_nn = Aeq1_nn.detach().numpy()
-        Aeq2_nn = Aeq2_nn.detach().numpy()
-        Aeq3_nn = Aeq3_nn.detach().numpy()
-        rhs_eq_nn = rhs_eq_nn.squeeze().detach().numpy()
-        a_out_nn = a_out_nn.squeeze().detach().numpy()
-        z = cp.Variable(Ain2_nn.shape[1])
-        beta = cp.Variable(Ain3_nn.shape[1], boolean=True)
-        # obj = cp.Minimize(0.)
-        obj = cp.Minimize(-a_out_nn@z - b_out_nn)
-        con = [
-            Ain1_nn@x0.detach().numpy() + Ain2_nn@z + Ain3_nn@beta <= rhs_in_nn,
-            Aeq1_nn@x0.detach().numpy() + Aeq2_nn@z + Aeq3_nn@beta == rhs_eq_nn,
-        ]
-        prob = cp.Problem(obj, con)
-        prob.solve(solver=cp.GUROBI, verbose=False)
-        z_opt_nn = a_out_nn@z.value + b_out_nn
-        print(z_opt_nn)
+            
+        def value_fun_wrapper(x0):
+            vf = ValueToOptimization.ValueFunction(sys)
+            (Q2_traj, Q3_traj, q2_traj, q3_traj,
+            Ain1_traj, Ain2_traj, Ain3_traj, brhs_in_traj, 
+            Aeq1_traj, Aeq2_traj, Aeq3_traj, brhs_eq_traj) = vf.traj_opt_x0xN(
+                Q, R, Z, q, r, z, 
+                Qt, Rt, Zt, qt, rt, zt, 
+                N, xN,
+                x_lo_traj, x_up_traj, u_lo, u_up,
+                x0=x0)        
+            Q2_traj = Q2_traj.detach().numpy()
+            q2_traj = q2_traj.detach().numpy()
+            Q3_traj = Q3_traj.detach().numpy()
+            q3_traj = q3_traj.detach().numpy()
+            Ain1_traj = Ain1_traj.detach().numpy()
+            Ain2_traj = Ain2_traj.detach().numpy()
+            Ain3_traj = Ain3_traj.detach().numpy()
+            brhs_in_traj = brhs_in_traj.detach().numpy()    
+            Aeq1_traj = Aeq1_traj.detach().numpy()
+            Aeq2_traj = Aeq2_traj.detach().numpy()
+            Aeq3_traj = Aeq3_traj.detach().numpy()
+            brhs_eq_traj = brhs_eq_traj.detach().numpy()
+            x = cp.Variable(Ain1_traj.shape[1])
+            s = cp.Variable(Ain2_traj.shape[1])
+            alpha = cp.Variable(Ain3_traj.shape[1],boolean=True)
+            obj = cp.Minimize(.5*cp.quad_form(s,Q2_traj) + q2_traj*s + .5*cp.quad_form(alpha,Q3_traj) + q3_traj*alpha)
+            con = [
+                Ain1_traj@x + Ain2_traj@s + Ain3_traj@alpha <= brhs_in_traj.squeeze(),
+                Aeq1_traj@x + Aeq2_traj@s + Aeq3_traj@alpha == brhs_eq_traj.squeeze()
+            ]
+            prob = cp.Problem(obj,con)
+            prob.solve(solver=cp.GUROBI, verbose=False)
+            value = obj.value
+            return value
 
+        x0 = torch.Tensor(y.value[:3]).type(sys.dtype)
+        value = value_fun_wrapper(x0)
         z_nn = self.model(x0).item()
-        print(z_nn)
         
-        # print(value - z_opt_nn)
-        # print(value - z_nn)
-        # print(epsilon)
-        
+        self.assertAlmostEqual(epsilon, value - z_nn, places=4)
+                
+        for i in range(200):
+            if i < 50:
+                x0_sub = torch.rand(3, dtype=sys.dtype)*(x_up - x_lo) + x_lo
+            else:
+                x0_sub = x0 + .2*torch.rand(3, dtype=sys.dtype)*(x_up - x_lo)
+                x0_sub = torch.max(torch.min(x0_sub,x_up),x_lo)
+            value_sub = value_fun_wrapper(x0_sub)
+            if type(value_sub) != type(None):
+                z_nn_sub = self.model(x0_sub).item()
+                epsilon_sub = value_sub - z_nn_sub
+                self.assertLessEqual(-epsilon_sub, -epsilon)
+                    
 
 if __name__ == '__main__':
     unittest.main()
