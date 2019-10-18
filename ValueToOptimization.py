1d4cfcebd460830382184e99c6a5a0253d2ab26d
Benoit Landry <ben.acc.landry@gmail.com>
2019-10-18 01:19:56 -0700

    adressing Hongkai comments

--- ValueToOptimization.py
+++ ValueToOptimization.py
@@ -34,11 +34,15 @@ class ValueFunction:
         self.x0 = None
         self.xN = None
         
+        self.xtraj = None
+        self.utraj = None
+        self.alphatraj = None
+        
     def set_cost(self, Q=None, R=None, Z=None, q=None, r=None, z=None):
         """
         Sets the parameters of the additive cost function (not including terminal state)
         
-        ∑(.5 xᵀ[n] Q x[n] + .5 uᵀ[n] R u[n] + .5 αᵀ[n] Z α[n] + qᵀx[n] + rᵀu[n] + zᵀα[n])
+        ∑(.5 (x[n]-xtraj[n])ᵀ Q (x[n]-xtraj[n]) + .5 (u[n]-utraj[n])ᵀ R (u[n]-utraj[n]) + .5 (α[n]-αtraj[n])ᵀ Z (α[n]-αtraj[n]) + qᵀ(x[n]-xtraj[n]) + rᵀ(u[n]-utraj[n]) + zᵀ(α[n]-αtraj[n]))
         
         for n = 0...N-1
         """
@@ -59,7 +63,7 @@ class ValueFunction:
         """
         Set the parameters of the terminal cost
         
-        .5 xᵀ[N] Qt x[N] + .5 uᵀ[N] Rt u[N] + .5 αᵀ[N] Zt α[N] + qtᵀx[N] + rtᵀu[N] + ztᵀα[N]
+        .5 (x[N]-xtraj[N])ᵀ Qt (x[N]-xtraj[N]) + .5 (u[N]-utraj[N])ᵀ Rt (u[N]-utraj[N]) + .5 (α[N]-αtraj[N])ᵀ Zt (α[N]-αtraj[N]) + qtᵀ(x[N]-xtraj[N]) + rtᵀ(u[N]-utraj[N]) + ztᵀ(α[N]-αtraj[N])
         """
         if type(Qt) != type(None):
             self.Qt = Qt.type(self.dtype)
@@ -96,13 +100,27 @@ class ValueFunction:
         if type(xN) != type(None):
             self.xN = xN.type(self.dtype)
 
+    def set_traj(self, xtraj=None, utraj=None, alphatraj=None):
+        """
+        Sets the desired trajectory (see description of set_cost and set_terminal_cost).
+        
+        @param xtraj the desired x trajectory as a statedim by N tensor
+        @param utraj the desired u trajectory as a inputdim by N tensor
+        @param alphatraj the desired x trajectory as a numdiscretestates by N tensor
+        """
+        if type(xtraj) != type(None):
+            self.xtraj = xtraj.type(self.dtype)
+        if type(utraj) != type(None):
+            self.utraj = utraj.type(self.dtype)
+        if type(alphatraj) != type(None):
+            self.alphatraj = alphatraj.type(self.dtype)
+
     def traj_opt_constraint(self, N):
         """
         Generates a trajectory optimization problem corresponding to the set constraints
         and objectives
         
-        min ∑(.5 xᵀ[n] Q x[n] + .5 uᵀ[n] R u[n] + .5 αᵀ[n] Z α[n] + qᵀx[n] + rᵀu[n] + zᵀα[n])
-                + .5 xᵀ[N] Qt x[N] + .5 uᵀ[N] Rt u[N] + .5 αᵀ[N] Zt α[N] + qtᵀx[N] + rtᵀu[N] + ztᵀα[N]
+        min ∑(.5 (x[n]-xtraj[n])ᵀ Q (x[n]-xtraj[n]) + .5 (u[n]-utraj[n])ᵀ R (u[n]-utraj[n]) + .5 (α[n]-αtraj[n])ᵀ Z (α[n]-αtraj[n]) + qᵀ(x[n]-xtraj[n]) + rᵀ(u[n]-utraj[n]) + zᵀ(α[n]-αtraj[n])) + .5 (x[N]-xtraj[N])ᵀ Qt (x[N]-xtraj[N]) + .5 (u[N]-utraj[N])ᵀ Rt (u[N]-utraj[N]) + .5 (α[N]-αtraj[N])ᵀ Zt (α[N]-αtraj[N]) + qtᵀ(x[N]-xtraj[N]) + rtᵀ(u[N]-utraj[N]) + ztᵀ(α[N]-αtraj[N])
         Ain1 x[n] + Ain2 u[n] + Ain3 x[n+1] + Ain4 u[n+1] + Ain5 α[n] ≤ rhs_in_dyn
         Aeq1 x[n] + Aeq2 u[n] + Aeq3 x[n+1] + Aeq4 u[n+1] + Aeq5 α[n] = rhs_eq_dyn
         x_lo ≤ x[n] ≤ x_up
@@ -114,13 +132,20 @@ class ValueFunction:
         the problem is returned in our standard MIQP form so that it can easily be passed to verification functions.
         Letting x = x[0], and s = x[1]...x[N]
         
-        min .5 sᵀ Q2 s + .5 αᵀ Q3 α + q2ᵀ s + q3ᵀ α
+        min .5 sᵀ Q2 s + .5 αᵀ Q3 α + q2ᵀ s + q3ᵀ α + c
         s.t. Ain1 x + Ain2 s + Ain3 α ≤ rhs_in
              Aeq1 x + Aeq2 s + Aeq3 α = rhs_eq
              α ∈ {0,1} (needs to be imposed externally)
              
-        @return Ain1, Ain2, Ain3, rhs_eq, Aeq1, Aeq2, Aeq3, rhs_eq, Q2, Q3, q2, q3
+        @return Ain1, Ain2, Ain3, rhs_eq, Aeq1, Aeq2, Aeq3, rhs_eq, Q2, Q3, q2, q3, c
         """
+        if type(self.xtraj) != type(None):
+            assert(self.xtraj.shape[1] == N-1)
+        if type(self.utraj) != type(None):
+            assert(self.utraj.shape[1] == N)
+        if type(self.alphatraj) != type(None):
+            assert(self.alphatraj.shape[1] == N)
+        
         # TODO: this needs to be rewritten with HybridLinearSystem api
         Ain1_dyn, Ain2_dyn, Ain3_dyn, Ain4_dyn, Ain5_dyn, rhs_in_dyn = self.sys.get_dyn_in()
         Aeq1_dyn, Aeq2_dyn, Aeq3_dyn, Aeq4_dyn, Aeq5_dyn, rhs_eq_dyn = self.sys.get_dyn_eq()
@@ -132,7 +157,6 @@ class ValueFunction:
         alphadim = adim*N
         
         # dynamics inequality constraints
-        # Ain1 x[n] + Ain2 u[n] + Ain3 x[n+1] + Ain4 u[n+1] + Ain5 α[n] ≤ rhs_in_dyn
         num_in_dyn = rhs_in_dyn.shape[0]
         s_in_dyn = torch.cat((Ain1_dyn, Ain2_dyn, Ain3_dyn, Ain4_dyn), 1)
         Ain = torch.zeros((N-1)*num_in_dyn,N*(xdim+udim),dtype=self.dtype)
@@ -146,7 +170,6 @@ class ValueFunction:
         Ain2 = Ain[:,xdim:]
         
         # dynamics equality constraints
-        # Aeq1 x[n] + Aeq2 u[n] + Aeq3 x[n+1] + Aeq4 u[n+1] + Aeq5 α[n] = rhs_eq_dyn
         num_eq_dyn = rhs_eq_dyn.shape[0]
         s_eq_dyn = torch.cat((Aeq1_dyn, Aeq2_dyn, Aeq3_dyn, Aeq4_dyn), 1)
         Aeq = torch.zeros((N-1)*num_eq_dyn,N*(xdim+udim),dtype=self.dtype)
@@ -160,48 +183,82 @@ class ValueFunction:
         Aeq2 = Aeq[:,xdim:]
         
         # costs 
-        # .5 xᵀ[n] Q x[n] + .5 uᵀ[n] R u[n] + .5 αᵀ[n] Z α[n] + qᵀx[n] + rᵀu[n] + zᵀα[n]
         Q2 = torch.zeros(sdim,sdim,dtype=self.dtype)
         q2 = torch.zeros(sdim,dtype=self.dtype)
         Q3 = torch.zeros(alphadim,alphadim,dtype=self.dtype)
         q3 = torch.zeros(alphadim,dtype=self.dtype)
+        c = 0.
         if type(self.R) != type(None):
-            Q2[:udim,:udim] = self.R
+            Q2[:udim,:udim] += self.R
+            if type(self.utraj) != type(None):
+                q2[:udim] -= self.utraj[:,0].T@self.R
+                c += .5*self.utraj[:,0].T@self.R@self.utraj[:,0]
         if type(self.r) != type(None):
-            q2[:udim] = self.r
+            q2[:udim] += self.r
+            if type(self.utraj) != type(None):
+                c -= self.r.T@self.utraj[:,0]
         for i in range(N-2):
             Qi = udim+i*(xdim+udim)
             Qip = udim+i*(xdim+udim)+xdim
             Ri = udim+xdim+i*(xdim+udim)
             Rip = udim+xdim+i*(xdim+udim)+udim
             if type(self.Q) != type(None):
-                Q2[Qi:Qip,Qi:Qip] = self.Q
+                Q2[Qi:Qip,Qi:Qip] += self.Q
+                if type(self.xtraj) != type(None):
+                    q2[Qi:Qip] -= self.xtraj[:,i].T@self.Q
+                    c += .5*self.xtraj[:,i].T@self.Q@self.xtraj[:,i]
             if type(self.R) != type(None):
-                Q2[Ri:Rip,Ri:Rip] = self.R
+                Q2[Ri:Rip,Ri:Rip] += self.R
+                if type(self.utraj) != type(None):
+                    q2[Ri:Rip] -= self.utraj[:,i+1].T@self.R
+                    c += .5*self.utraj[:,i+1].T@self.R@self.utraj[:,i+1]
             if type(self.q) != type(None):
-                q2[Qi:Qip] = self.q
+                q2[Qi:Qip] += self.q
+                if type(self.xtraj) != type(None):
+                    c -= self.q.T@self.xtraj[:,i]
             if type(self.r) != type(None):            
-                q2[Ri:Rip] = self.r
-        for i in range(N):
+                q2[Ri:Rip] += self.r
+                if type(self.utraj) != type(None):
+                    c -= self.r.T@self.utraj[:,i+1]
+        for i in range(N-1):
             if type(self.Z) != type(None):
-                Q3[i*adim:(i+1)*adim,i*adim:(i+1)*adim] = self.Z
+                Q3[i*adim:(i+1)*adim,i*adim:(i+1)*adim] += self.Z
+                if type(self.alphatraj) != type(None):
+                    q3[i*adim:(i+1)*adim] -= self.alphatraj[:,i].T@self.Z
+                    c += .5*self.alphatraj[:,i].T@self.Z@self.alphatraj[:,i]                
             if type(self.z) != type(None):
-                q3[i*adim:(i+1)*adim] = self.z
+                q3[i*adim:(i+1)*adim] += self.z
+                if type(self.alphatraj) != type(None):
+                    c -= self.z.T@self.alphatraj[:,i]
         
-        # .5 xᵀ[N] Qt x[N] + .5 uᵀ[N] Rt u[N] + .5 αᵀ[N] Zt α[N] + qtᵀx[N] + rtᵀu[N] + ztᵀα[N]
         if type(self.Qt) != type(None):
-            Q2[-(xdim+udim):-udim,-(xdim+udim):-udim] = self.Qt
+            Q2[-(xdim+udim):-udim,-(xdim+udim):-udim] += self.Qt
+            if type(self.xtraj) != type(None):
+                q2[-(xdim+udim):-udim] -= self.xtraj[:,-1].T@self.Qt
+                c += .5*self.xtraj[:,-1].T@self.Qt@self.xtraj[:,-1]
         if type(self.Rt) != type(None):
-            Q2[-udim:,-udim:] = self.Rt
+            Q2[-udim:,-udim:] += self.Rt
+            if type(self.utraj) != type(None):
+                q2[-udim:] -= self.utraj[:,-1].T@self.Rt
+                c += .5*self.utraj[:,-1].T@self.Rt@self.utraj[:,-1]
         if type(self.qt) != type(None):
-            q2[-(xdim+udim):-udim] = self.qt
+            q2[-(xdim+udim):-udim] += self.qt
+            if type(self.xtraj) != type(None):
+                c -= self.qt.T@self.xtraj[:,-1]
         if type(self.rt) != type(None):
-            q2[-udim:] = self.rt
+            q2[-udim:] += self.rt
+            if type(self.utraj) != type(None):
+                c -= self.rt.T@self.utraj[:,-1]
         if type(self.Zt) != type(None):
-            Q3[-adim:,-adim:] = self.Zt
+            Q3[-adim:,-adim:] += self.Zt
+            if type(self.alphatraj) != type(None):
+                q3[-adim:] -= self.alphatraj[:,-1].T@self.Zt
+                c += .5*self.alphatraj[:,-1].T@self.Zt@self.alphatraj[:,-1]
         if type(self.zt) != type(None):
-            q3[-adim:] = self.zt
-
+            q3[-adim:] += self.zt
+            if type(self.alphatraj) != type(None):
+                c -= self.zt.T@self.alphatraj[:,-1]
+                
         # state and input constraints
         # x_lo ≤ x[n] ≤ x_up
         # u_lo ≤ u[n] ≤ u_up
@@ -239,4 +296,4 @@ class ValueFunction:
             Aeq3 = torch.cat((Aeq3,torch.zeros(xdim,alphadim,dtype=self.dtype)),0)
             rhs_eq = torch.cat((rhs_eq,self.xN))
 
-        return(Ain1, Ain2, Ain3, rhs_in, Aeq1, Aeq2, Aeq3, rhs_eq, Q2, Q3, q2, q3)
+        return(Ain1, Ain2, Ain3, rhs_in, Aeq1, Aeq2, Aeq3, rhs_eq, Q2, Q3, q2, q3, c)
--- test/ValueToOptimizationTest.py
+++ test/ValueToOptimizationTest.py
@@ -26,13 +26,13 @@ class ValueToOptimizationTest(unittest.TestCase):
         traj_opt = vf.traj_opt_constraint(N)
         (Ain1, Ain2, Ain3, rhs_in, 
         Aeq1, Aeq2, Aeq3, rhs_eq, 
-        Q2, Q3, q2, q3) = torch_to_numpy(traj_opt)
+        Q2, Q3, q2, q3, c) = torch_to_numpy(traj_opt)
     
         x = cp.Variable(Ain1.shape[1])
         s = cp.Variable(Ain2.shape[1])
         z = cp.Variable(Ain3.shape[1],boolean=True)
 
-        obj = cp.Minimize(.5*cp.quad_form(s,Q2) + .5*cp.quad_form(z,Q3) + q2.T@s + q3.T@z)    
+        obj = cp.Minimize(.5*cp.quad_form(s,Q2) + .5*cp.quad_form(z,Q3) + q2.T@s + q3.T@z + c)    
         con = [
             Ain1@x + Ain2@s + Ain3@z <= rhs_in,
             Aeq1@x + Aeq2@s + Aeq3@z == rhs_eq,
@@ -71,7 +71,7 @@ class ValueToOptimizationTest(unittest.TestCase):
         N = 3
         (Ain1, Ain2, Ain3, rhs_in, 
         Aeq1, Aeq2, Aeq3, rhs_eq, 
-        Q2, Q3, q2, q3) = vf.traj_opt_constraint(N)
+        Q2, Q3, q2, q3, c) = vf.traj_opt_constraint(N)
         
         x = torch.rand(3,N-1).type(sys.dtype)
         u0 = torch.rand(1).type(sys.dtype)
@@ -98,13 +98,68 @@ class ValueToOptimizationTest(unittest.TestCase):
         obj_exp += .5*alpha[:,i].t()@Zt@alpha[:,i]
         obj_exp += alpha[:,i].t()@zt
         
-        obj = .5*s@Q2@s.t() + .5*alpha@Q3@alpha.t() + s@q2 + alpha@q3
+        obj = .5*s@Q2@s.t() + .5*alpha@Q3@alpha.t() + s@q2 + alpha@q3 + c
         
         # print(obj_exp.item())
         # print(obj.item())
 
         self.assertAlmostEqual(obj_exp.item(), obj.item())
+        
+    def test_trajopt_obj_trajsetpoint(self):
+        sys = BallPaddleSystem.BallPaddleSystem(dt=.01)
+        vf = ValueToOptimization.ValueFunction(sys)
 
+        N = 5
+        [Q,R,Z,q,r,z,Qt,Rt,Zt,qt,rt,zt] = sys.get_simple_trajopt_cost()
+        utraj0 = torch.rand(1,1).type(sys.dtype)
+        utraj = torch.rand(1,N-1).type(sys.dtype)
+        xtraj = torch.rand(3,N-1).type(sys.dtype)
+        alphatraj = torch.rand(1,N).type(sys.dtype)
+        vf.set_cost(Q=Q,R=R,Z=Z,q=q,r=r,z=z)
+        vf.set_terminal_cost(Qt=Qt,Rt=Rt,Zt=Zt,qt=qt,rt=rt,zt=zt)
+        torch.cat((utraj0,utraj),1)
+        vf.set_traj(xtraj=xtraj,utraj=torch.cat((utraj0,utraj),1),alphatraj=alphatraj)
+
+        x0 = torch.Tensor([0.,.1,0.])
+        xN = torch.Tensor([0.,.075,0.])
+        vf.set_constraints(x0=x0,xN=xN)
+
+        (Ain1, Ain2, Ain3, rhs_in, 
+        Aeq1, Aeq2, Aeq3, rhs_eq, 
+        Q2, Q3, q2, q3, c) = vf.traj_opt_constraint(N)
+               
+        x = torch.rand(3,N-1).type(sys.dtype)
+        u0 = torch.rand(1).type(sys.dtype)
+        u = torch.rand(1,N-1).type(sys.dtype)
+        s = torch.cat((u0,torch.cat((x,u),0).t().reshape(-1)))
+        alpha = torch.rand(1,N).type(sys.dtype)
+               
+        obj_exp = .5*(u0-utraj0).t()@R@(u0-utraj0).t()
+        obj_exp += (u0-utraj0).t()@r
+        for i in range(N-2):
+            obj_exp += .5*(x[:,i]-xtraj[:,i]).t()@Q@(x[:,i]-xtraj[:,i])
+            obj_exp += (x[:,i]-xtraj[:,i]).t()@q
+            obj_exp += .5*(u[:,i]-utraj[:,i]).t()@R@(u[:,i]-utraj[:,i])
+            obj_exp += (u[:,i]-utraj[:,i]).t()@r
+        i = N-2
+        obj_exp += .5*(x[:,i]-xtraj[:,i]).t()@Qt@(x[:,i]-xtraj[:,i])
+        obj_exp += (x[:,i]-xtraj[:,i]).t()@qt
+        obj_exp += .5*(u[:,i]-utraj[:,i]).t()@Rt@(u[:,i]-utraj[:,i])
+        obj_exp += (u[:,i]-utraj[:,i]).t()@rt
+        for i in range(N-1):
+            obj_exp += .5*(alpha[:,i]-alphatraj[:,i]).t()@Z@(alpha[:,i]-alphatraj[:,i])
+            obj_exp += (alpha[:,i]-alphatraj[:,i]).t()@z
+        i = N-1
+        obj_exp += .5*(alpha[:,i]-alphatraj[:,i]).t()@Zt@(alpha[:,i]-alphatraj[:,i])
+        obj_exp += (alpha[:,i]-alphatraj[:,i]).t()@zt
+       
+        obj = .5*s@Q2@s.t() + .5*alpha@Q3@alpha.t() + s@q2 + alpha@q3 + c
+       
+        # print(obj_exp.item())
+        # print(obj.item())
+
+        self.assertAlmostEqual(obj_exp.item(), obj.item())
+       
 
 if __name__ == '__main__':
     unittest.main()
