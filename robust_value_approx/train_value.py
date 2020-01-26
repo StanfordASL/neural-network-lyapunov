15f822284a4c755eeb1a147eac78bfb3163a8436
blandry <ben.acc.landry@gmail.com>
2020-01-25 22:14:53 -0800

    lots of plotting functions

--- robust_value_approx/train_value.py
+++ robust_value_approx/train_value.py
@@ -8,26 +8,26 @@ from torch.utils.tensorboard import SummaryWriter
 
 class AdversarialWithBaselineTrainingOptions:
     def __init__(self):
-        # number of iteration to be done
+        # total number of iteration to be done
         self.num_iter_desired = 1000
+        # number of training steps between each generation of new adv samples
+        self.num_steps_between_sampling = 100
+        # number of random samples to initialize the buffer with
+        self.init_buffer_size = 0
+        # number of additional random samples generated each sampling
+        self.num_rand_extra = 0
+        # number of adversarial optimization run at each sampling
+        self.num_x_adv_opt = 1
+        # maximum number of iterations to find adversarial samples
+        self.x_adv_max_iter = 5
+        # convergence tolerance for adversarial samples
+        self.x_adv_conv_tol = 1e-5
+        # learning rate used to find adversarial examples
+        self.x_adv_lr = .001
         # batch size during training
         self.batch_size = 10
         # size of the buffer when training
         self.max_buffer_size = 100000
-        # number of random samples to initialize the buffer with
-        self.init_buffer_size = 1
-        # number of training steps between each generation of new adv samples
-        self.sample_refresh_rate = 100
-        # number of additional random samples generated each data gen
-        self.num_rand_extra = 0
-        # maximum number of iterations to find adversarial examples
-        self.x_adv_max_iter = 5
-        # convergence tolerance for adversarial examples
-        self.x_adv_conv_tol = 1e-3
-        # learning rate used to find adversarial examples
-        self.x_adv_lr = .001
-        # x_adv0 = (1 - x_adv0_noise)*x_adv[rand] + x_adv0_noise*x_rand
-        self.x_adv0_noise = 1.
 
 
 class AdversarialWithBaseline:
@@ -94,8 +94,14 @@ class AdversarialWithBaseline:
         self.robust_mse = torch.nn.MSELoss(reduction="mean")
         self.as_generator = adversarial_sample.AdversarialSampleGenerator(
             vf, x0_lo, x0_up)
+        # logging
         self.writer = SummaryWriter()
         self.num_total_iter_done = 0
+        self.x_adv_opt_log = []
+        self.robust_loss_log = []
+        self.baseline_loss_log = []
+        self.robust_val_loss_log = []
+        self.baseline_val_loss_log = []
 
     def get_random_x0(self):
         """
@@ -121,127 +127,189 @@ class AdversarialWithBaseline:
                 k += 1
         return(rand_data, rand_label)
 
+    def get_adversarial_samples(self, opt):
+        """
+        @param TODO
+        """
+        assert(isinstance(opt, AdversarialWithBaselineTrainingOptions))
+        adv_data_ = torch.Tensor(0, self.x_dim).type(self.dtype)
+        adv_label_ = torch.Tensor(0, 1).type(self.dtype)
+        self.x_adv_opt_log = []
+        for k in range(opt.num_x_adv_opt):
+            x_adv0 = self.get_random_x0()
+            adv = self.as_generator.get_squared_bound_sample(
+                self.robust_model,
+                max_iter=opt.x_adv_max_iter,
+                conv_tol=opt.x_adv_conv_tol,
+                learning_rate=opt.x_adv_lr,
+                x_adv0=x_adv0)
+            (_, adv_data_k, adv_label_k, _) = adv
+            if adv_data_k.shape[0] <= 1:
+                print("Warning: no descent on adversarial sample")
+            adv_data_ = torch.cat((adv_data_, adv_data_k), axis=0)
+            adv_label_ = torch.cat((adv_label_, adv_label_k), axis=0)
+            # just for plotting
+            with torch.no_grad():
+                self.x_adv_opt_log.append(
+                    (adv_data_k.clone(), adv_label_k.clone()))
+        return(adv_data_, adv_label_)
+
     def train(self, opt, logging=True):
         """
         @param opt instance of AdversarialWithBaselineTrainingOptions
         @param logging Boolean whether or not to log with tensorboard
         """
-        x_adv_opt = self.get_random_x0()
         assert(isinstance(opt, AdversarialWithBaselineTrainingOptions))
-        for iter_num in range(opt.num_iter_desired):
+        if self.adv_data_buffer.shape[0] < opt.init_buffer_size:
+            (rand_data_, rand_label_) = self.get_random_samples(
+                opt.init_buffer_size -
+                self.adv_data_buffer.shape[0])
+            self.adv_data_buffer = torch.cat(
+                (self.adv_data_buffer, rand_data_), axis=0)
+            self.adv_label_buffer = torch.cat(
+                (self.adv_label_buffer, rand_label_), axis=0)
+            self.rand_data_buffer = torch.cat(
+                (self.rand_data_buffer, rand_data_), axis=0)
+            self.rand_label_buffer = torch.cat(
+                (self.rand_label_buffer, rand_label_), axis=0)
+        iter_num = 0
+        while iter_num < opt.num_iter_desired:
             assert(self.adv_data_buffer.shape[0]
                    == self.rand_data_buffer.shape[0])
             assert(self.adv_label_buffer.shape[0]
                    == self.rand_label_buffer.shape[0])
-            if self.adv_data_buffer.shape[0] < opt.init_buffer_size:
-                (rand_data_, rand_label_) = self.get_random_samples(
-                    opt.init_buffer_size -
-                    self.adv_data_buffer.shape[0])
-                self.adv_data_buffer = torch.cat(
-                    (self.adv_data_buffer, rand_data_), axis=0)
-                self.adv_label_buffer = torch.cat(
-                    (self.adv_label_buffer, rand_label_), axis=0)
-                self.rand_data_buffer = torch.cat(
-                    (self.rand_data_buffer, rand_data_), axis=0)
-                self.rand_label_buffer = torch.cat(
-                    (self.rand_label_buffer, rand_label_), axis=0)
-            if iter_num > 0 and iter_num % opt.sample_refresh_rate == 0:
-                x_adv0 = (1. - opt.x_adv0_noise) * x_adv_opt +\
-                    opt.x_adv0_noise * self.get_random_x0()
-                adv = self.as_generator.get_squared_bound_sample(
-                    self.robust_model,
-                    max_iter=opt.x_adv_max_iter,
-                    conv_tol=opt.x_adv_conv_tol,
-                    learning_rate=opt.x_adv_lr,
-                    x_adv0=x_adv0)
-                (_, adv_data_, adv_label_, _) = adv
-                x_adv_opt = adv_data_[-1, :]
-                num_new_adv_samples = adv_data_.shape[0]
-                if num_new_adv_samples <= 1:
-                    print("Warning: no descent on adversarial sample")
-                # allowed to solve as many MIQP's but randomly
-                (rand_data_, rand_label_) = self.get_random_samples(
-                    num_new_adv_samples)
-                if (num_new_adv_samples +
-                        self.adv_data_buffer.shape[0] > opt.max_buffer_size):
-                    self.adv_data_buffer = self.adv_data_buffer[
-                        num_new_adv_samples:, :]
-                    self.adv_label_buffer = self.adv_label_buffer[
-                        num_new_adv_samples:, :]
-                    self.rand_data_buffer = self.rand_data_buffer[
-                        num_new_adv_samples:, :]
-                    self.rand_label_buffer = self.rand_label_buffer[
-                        num_new_adv_samples:, :]
-                self.adv_data_buffer = torch.cat(
-                    (self.adv_data_buffer, adv_data_), axis=0)
-                self.adv_label_buffer = torch.cat(
-                    (self.adv_label_buffer, adv_label_), axis=0)
-                self.rand_data_buffer = torch.cat(
-                    (self.rand_data_buffer, rand_data_), axis=0)
-                self.rand_label_buffer = torch.cat(
-                    (self.rand_label_buffer, rand_label_), axis=0)
-                # adding additional random samples to both
-                if (opt.num_rand_extra +
-                        self.adv_data_buffer.shape[0] > opt.max_buffer_size):
-                    self.adv_data_buffer = self.adv_data_buffer[
-                        opt.num_rand_extra:, :]
-                    self.adv_label_buffer = self.adv_label_buffer[
-                        opt.num_rand_extra:, :]
-                    self.rand_data_buffer = self.rand_data_buffer[
-                        opt.num_rand_extra:, :]
-                    self.rand_label_buffer = self.rand_label_buffer[
-                        opt.num_rand_extra:, :]
-                (rand_data_, rand_label_) = self.get_random_samples(
-                    opt.num_rand_extra)
-                self.adv_data_buffer = torch.cat(
-                    (self.adv_data_buffer, rand_data_), axis=0)
-                self.adv_label_buffer = torch.cat(
-                    (self.adv_label_buffer, rand_label_), axis=0)
-                self.rand_data_buffer = torch.cat(
-                    (self.rand_data_buffer, rand_data_), axis=0)
-                self.rand_label_buffer = torch.cat(
-                    (self.rand_label_buffer, rand_label_), axis=0)
-            # pick batch size data from buffer randomly
-            assert(self.adv_data_buffer.shape[0]
-                   == self.rand_data_buffer.shape[0])
-            assert(self.adv_label_buffer.shape[0]
-                   == self.rand_label_buffer.shape[0])
-            data_i = np.random.choice(
-                self.adv_data_buffer.shape[0],
-                min(self.adv_data_buffer.shape[0], opt.batch_size),
-                replace=False)
-            adv_data = self.adv_data_buffer[data_i, :]
-            adv_label = self.adv_label_buffer[data_i, :]
-            y_pred_adv = self.robust_model(adv_data)
-            robust_loss = self.robust_mse(y_pred_adv, adv_label)
-            self.robust_optimizer.zero_grad()
-            robust_loss.backward()
-            self.robust_optimizer.step()
-            # pick batch size data from buffer randomly
-            rand_data = self.rand_data_buffer[data_i, :]
-            rand_label = self.rand_label_buffer[data_i, :]
-            y_pred_rand = self.baseline_model(rand_data)
-            baseline_loss = self.baseline_mse(y_pred_rand, rand_label)
-            self.baseline_optimizer.zero_grad()
-            baseline_loss.backward()
-            self.baseline_optimizer.step()
-            if logging:
-                self.writer.add_scalars('Train',
-                                        {'baseline': baseline_loss.item(),
-                                         'robust': robust_loss.item()},
-                                        self.num_total_iter_done)
-                with torch.no_grad():
-                    baseline_validation_loss = self.baseline_mse(
-                        self.baseline_model(self.x_samples_validation),
-                        self.v_samples_validation)
-                    robust_validation_loss = self.robust_mse(
-                        self.robust_model(self.x_samples_validation),
-                        self.v_samples_validation)
-                bvl = baseline_validation_loss.item()
-                rvl = robust_validation_loss.item()
-                self.writer.add_scalars('Validation',
-                                        {'baseline': bvl,
-                                         'robust': rvl},
-                                        self.num_total_iter_done)
-            # keeping track of iterations
-            self.num_total_iter_done += 1
+            (adv_data_, adv_label_) = self.get_adversarial_samples(opt)
+            num_new_adv_samples = adv_data_.shape[0]
+            (rand_data_, rand_label_) = self.get_random_samples(
+                num_new_adv_samples)
+            if (num_new_adv_samples +
+                    self.adv_data_buffer.shape[0] > opt.max_buffer_size):
+                self.adv_data_buffer = self.adv_data_buffer[
+                    num_new_adv_samples:, :]
+                self.adv_label_buffer = self.adv_label_buffer[
+                    num_new_adv_samples:, :]
+                self.rand_data_buffer = self.rand_data_buffer[
+                    num_new_adv_samples:, :]
+                self.rand_label_buffer = self.rand_label_buffer[
+                    num_new_adv_samples:, :]
+            self.adv_data_buffer = torch.cat(
+                (self.adv_data_buffer, adv_data_), axis=0)
+            self.adv_label_buffer = torch.cat(
+                (self.adv_label_buffer, adv_label_), axis=0)
+            self.rand_data_buffer = torch.cat(
+                (self.rand_data_buffer, rand_data_), axis=0)
+            self.rand_label_buffer = torch.cat(
+                (self.rand_label_buffer, rand_label_), axis=0)
+            # adding additional random samples to both
+            (rand_data_, rand_label_) = self.get_random_samples(
+                opt.num_rand_extra)
+            if (opt.num_rand_extra +
+                    self.adv_data_buffer.shape[0] > opt.max_buffer_size):
+                self.adv_data_buffer = self.adv_data_buffer[
+                    opt.num_rand_extra:, :]
+                self.adv_label_buffer = self.adv_label_buffer[
+                    opt.num_rand_extra:, :]
+                self.rand_data_buffer = self.rand_data_buffer[
+                    opt.num_rand_extra:, :]
+                self.rand_label_buffer = self.rand_label_buffer[
+                    opt.num_rand_extra:, :]
+            self.adv_data_buffer = torch.cat(
+                (self.adv_data_buffer, rand_data_), axis=0)
+            self.adv_label_buffer = torch.cat(
+                (self.adv_label_buffer, rand_label_), axis=0)
+            self.rand_data_buffer = torch.cat(
+                (self.rand_data_buffer, rand_data_), axis=0)
+            self.rand_label_buffer = torch.cat(
+                (self.rand_label_buffer, rand_label_), axis=0)
+            for learning_iter_num in range(opt.num_steps_between_sampling):
+                assert(self.adv_data_buffer.shape[0]
+                       == self.rand_data_buffer.shape[0])
+                assert(self.adv_label_buffer.shape[0]
+                       == self.rand_label_buffer.shape[0])
+                data_i = np.random.choice(
+                    self.adv_data_buffer.shape[0],
+                    min(self.adv_data_buffer.shape[0], opt.batch_size),
+                    replace=False)
+                adv_data = self.adv_data_buffer[data_i, :]
+                adv_label = self.adv_label_buffer[data_i, :]
+                y_pred_adv = self.robust_model(adv_data)
+                robust_loss = self.robust_mse(y_pred_adv, adv_label)
+                self.robust_optimizer.zero_grad()
+                robust_loss.backward()
+                self.robust_optimizer.step()
+                # pick batch size data from buffer randomly
+                rand_data = self.rand_data_buffer[data_i, :]
+                rand_label = self.rand_label_buffer[data_i, :]
+                y_pred_rand = self.baseline_model(rand_data)
+                baseline_loss = self.baseline_mse(y_pred_rand, rand_label)
+                self.baseline_optimizer.zero_grad()
+                baseline_loss.backward()
+                self.baseline_optimizer.step()
+                if logging:
+                    self.writer.add_scalars('Train',
+                                            {'baseline': baseline_loss.item(),
+                                             'robust': robust_loss.item()},
+                                            self.num_total_iter_done)
+                    with torch.no_grad():
+                        baseline_validation_loss = self.baseline_mse(
+                            self.baseline_model(self.x_samples_validation),
+                            self.v_samples_validation)
+                        robust_validation_loss = self.robust_mse(
+                            self.robust_model(self.x_samples_validation),
+                            self.v_samples_validation)
+                    bvl = baseline_validation_loss.item()
+                    rvl = robust_validation_loss.item()
+                    self.writer.add_scalars('Validation',
+                                            {'baseline': bvl,
+                                             'robust': rvl},
+                                            self.num_total_iter_done)
+                    self.writer.flush()
+                    self.robust_loss_log.append(robust_loss.item())
+                    self.baseline_loss_log.append(baseline_loss.item())
+                    self.robust_val_loss_log.append(rvl)
+                    self.baseline_val_loss_log.append(bvl)
+                # keeping track of iterations
+                self.num_total_iter_done += 1
+                iter_num += 1
+                if iter_num >= opt.num_iter_desired:
+                    break
+
+    def get_state(self):
+        """
+        Returns various states of the learning process for logging
+        """
+        with torch.no_grad():
+            state = dict()
+            state["adv_data_buffer"] = self.adv_data_buffer.clone()
+            state["adv_label_buffer"] = self.adv_label_buffer.clone()
+            state["rand_data_buffer"] = self.rand_data_buffer.clone()
+            state["rand_label_buffer"] = self.rand_label_buffer.clone()
+            state["robust_loss_log"] = copy.deepcopy(self.robust_loss_log)
+            state["robust_val_loss_log"] = copy.deepcopy(
+                self.robust_val_loss_log)
+            state["robust_buffer_loss"] = torch.pow(
+                self.adv_label_buffer.squeeze() -\
+                self.robust_model(self.adv_data_buffer).squeeze(), 2).clone()
+            state["robust_val_loss"] = torch.pow(
+                self.v_samples_validation.squeeze() -\
+                self.robust_model(
+                    self.x_samples_validation).squeeze(), 2).clone()
+            state["baseline_loss_log"] = copy.deepcopy(self.baseline_loss_log)
+            state["baseline_val_loss_log"] = copy.deepcopy(
+                self.baseline_val_loss_log)
+            state["baseline_buffer_loss"] = torch.pow(
+                self.rand_label_buffer.squeeze() -\
+                self.baseline_model(
+                    self.rand_data_buffer).squeeze(), 2).clone()
+            state["baseline_val_loss"] = torch.pow(
+                self.v_samples_validation.squeeze() -\
+                self.baseline_model(
+                    self.x_samples_validation).squeeze(), 2).clone()
+            x_adv_opt_loss_log = []
+            for (x, label) in self.x_adv_opt_log:
+                x_adv_opt_loss_log.append(
+                    (x.clone(),
+                     torch.pow(label.squeeze() -\
+                        self.robust_model(x).squeeze(), 2).clone()))
+            state["x_adv_opt_loss_log"] = x_adv_opt_loss_log
+        return state
