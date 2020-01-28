56e88c77c7190a2ea9186a784e82aac571f5658b
blandry <ben.acc.landry@gmail.com>
2020-01-28 13:29:47 -0800

    abort bilevel opt if running into infeasible problems

--- robust_value_approx/adversarial_sample.py
+++ robust_value_approx/adversarial_sample.py
@@ -312,6 +312,9 @@ class AdversarialSampleGenerator:
         for i in range(max_iter):
             (prob, x, s, alpha) = self.setup_val_opt(x_val=x_adv)
             prob.gurobi_model.optimize()
+            if prob.gurobi_model.solCount < 1:
+                print("Warning: adversarial sample became infeasible")
+                break
             Vx = prob.compute_objective_from_mip_data_and_solution(
                 penalty=penalty)
             nx = model(x_adv)
