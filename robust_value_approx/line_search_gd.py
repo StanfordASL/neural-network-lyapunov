7484ea82dbe6d7994bd2cbeac02a6b3cf1e2b49e
hongkai-dai <daihongkai@gmail.com>
2020-02-22 20:56:02 -0800

    Add a gradient descent optimizer with line search. The step size should satisfy Armijo's rule.

---
+++ robust_value_approx/line_search_gd.py
@@ -0,0 +1,153 @@
+import torch
+from torch.optim.optimizer import Optimizer, required
+
+
+class LineSearchGD(Optimizer):
+    r"""Implements gradient descent (optionally with momentum) with line search
+    to find the step size. In each iteration, the step size should satisfy
+    Armijo's rule
+    f(x+αp) ≤ f(x)+c₁αpᵀ∇ₓf
+    where α is the step size.
+
+    Nesterov momentum is based on the formula from
+    `On the importance of initialization and momentum in deep learning`__.
+
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate
+        momentum (float, optional): momentum factor (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov (bool, optional): enables Nesterov momentum (default: False)
+        loss_minimal_decrement (float, optional): c1 in Armijo's rule.
+        minimal_step_size (float, optional): the minimal step size in each
+        iteration.
+        step_size_reduction. If step size α doesn't satisfy Armijo's rule, then
+        try step_size_reduction * α.
+
+    Example:
+        >>> optimizer = torch.optim.LineSearchGD(model.parameters(), lr=0.1,
+                momentum=0.9)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+
+    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+
+    .. note::
+        The implementation of SGD with Momentum/Nesterov subtly differs from
+        Sutskever et. al. and implementations in some other frameworks.
+
+        Considering the specific case of Momentum, the update can be written as
+
+        .. math::
+                  v_{t+1} = \mu * v_{t} + g_{t+1} \\
+                  p_{t+1} = p_{t} - lr * v_{t+1}
+
+        where p, g, v and :math:`\mu` denote the parameters, gradient,
+        velocity, and momentum respectively.
+
+        This is in contrast to Sutskever et. al. and
+        other frameworks which employ an update of the form
+
+        .. math::
+             v_{t+1} = \mu * v_{t} + lr * g_{t+1} \\
+             p_{t+1} = p_{t} - v_{t+1}
+
+        The Nesterov version is analogously modified.
+    """
+
+    def __init__(self, params, lr=required, momentum=0, dampening=0,
+                 weight_decay=0, nesterov=False, min_step_size=1e-8,
+                 loss_minimal_decrement=1e-4, step_size_reduction=0.2):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError(f"Invalid weight_decay value: {weight_decay}")
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, nesterov=nesterov,
+                        min_step_size=min_step_size,
+                        loss_minimal_decrement=loss_minimal_decrement,
+                        step_size_reduction=step_size_reduction)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError(
+                "Nesterov momentum requires a momentum and zero dampening")
+        super(LineSearchGD, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(LineSearchGD, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('nesterov', False)
+
+    def add_grad(self, p, t, d_p):
+        p.data.add_(t, d_p)
+
+    def directional_evaluate(self, closure, p, t, d_p):
+        for i in range(len(p)):
+            self.add_grad(p[i], t, d_p[i])
+        loss = closure()
+        return loss
+
+    def line_search(self, loss0, closure, p, t, d_p):
+        for group in self.param_groups:
+            loss_minimal_decrement = group['loss_minimal_decrement']
+            step_size_reduction = group['step_size_reduction']
+            min_step_size = group['min_step_size']
+
+        increment = loss_minimal_decrement * torch.sum(torch.stack(
+            [torch.sum(p[i].grad * d_p[i]) for i in range(len(p))]))
+        t_prev = 0
+        while t > min_step_size:
+            loss = self.directional_evaluate(closure, p, t - t_prev, d_p)
+            if loss[0] <= loss0[0] + t * increment:
+                return loss
+            t_prev = t
+            t *= step_size_reduction
+        return loss
+
+    def step(self, closure, loss0):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        p_all = []
+        dp_all = []
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+            lr = group['lr']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if weight_decay != 0:
+                    d_p.add_(weight_decay, p.data)
+                if momentum != 0:
+                    param_state = self.state[p]
+                    if 'momentum_buffer' not in param_state:
+                        buf = param_state['momentum_buffer'] = \
+                            torch.clone(d_p).detach()
+                    else:
+                        buf = param_state['momentum_buffer']
+                        buf.mul_(momentum).add_(1 - dampening, d_p)
+                    if nesterov:
+                        d_p = d_p.add(momentum, buf)
+                    else:
+                        d_p = buf
+                p_all.append(p)
+                dp_all.append(-d_p)
+        loss = self.line_search(loss0, closure, p_all, lr, dp_all)
+        return loss
---
+++ robust_value_approx/test/test_line_search_gd.py
@@ -0,0 +1,162 @@
+import robust_value_approx.line_search_gd as line_search_gd
+
+import torch
+import numpy as np
+
+import unittest
+
+
+class TestLineSearchGD(unittest.TestCase):
+    def test(self):
+        """
+        Test a simple linear network with quadratic loss.
+        The loss can be written as aᵀXa + 2baᵀx+n*bᵀb, where X = ∑ᵢ xᵢxᵢᵀ,
+        x = ∑ᵢ xᵢ. a, b are the weight/bias of the linear layer.
+        """
+        dtype = torch.float64
+        network = torch.nn.Linear(2, 1).type(dtype)
+
+        def reset_network():
+            network.weight.data = torch.tensor([[1., 2.]], dtype=dtype)
+            network.bias.data = torch.tensor([3.], dtype=dtype)
+        reset_network()
+
+        def param_flat():
+            return torch.cat([network.weight.reshape((-1,)), network.bias])
+
+        def grad_flat():
+            return torch.cat(
+                [network.weight.grad.reshape((-1,)), network.bias.grad])
+
+        x = torch.tensor([[1, 2], [3, 4], [5, 6]], dtype=torch.float64)
+
+        def loss_fun(a, b):
+            y = x @ a.reshape((-1, 1)) + b
+            return [y.squeeze() @ y.squeeze()]
+
+        def grad_fun(a, b):
+            with torch.no_grad():
+                X = torch.zeros((2, 2), dtype=dtype)
+                for i in range(x.shape[0]):
+                    X += x[i].reshape((-1, 1)) @ x[i].reshape((1, -1))
+                x_sum = torch.sum(x, axis=0)
+
+                return torch.cat(
+                    [(2 * a.reshape((1, -1)) @ X).squeeze() + 2 * b * x_sum,
+                     2 * a @ x_sum + 2 * b * x.shape[0]])
+
+        def closure():
+            y = network(x)
+            return [y.squeeze() @ y.squeeze()]
+
+        loss0 = closure()
+        network.zero_grad()
+        loss0[0].backward()
+        np.testing.assert_allclose(
+            grad_flat().detach().numpy(),
+            grad_fun(network.weight.data.squeeze(), network.bias).
+            detach().numpy())
+        # Take a single step
+        lr = 1e-3
+        loss_minimal_decrement = 1e-4
+        dut1 = line_search_gd.LineSearchGD(
+            network.parameters(), lr=lr, momentum=0, min_step_size=1e-8,
+            loss_minimal_decrement=loss_minimal_decrement,
+            step_size_reduction=0.2)
+        a0 = network.weight.data.clone().squeeze()
+        b0 = network.bias.clone()
+        grad0 = grad_fun(a0, b0)
+        a1 = a0 - grad0[:2] * lr
+        b1 = b0 - grad0[2] * lr
+        loss1 = loss_fun(a1, b1)
+        assert(loss1[0] <= loss0[0] -
+               loss_minimal_decrement * lr * grad0 @ grad0)
+        dut1.step(closure, loss0)
+        np.testing.assert_allclose(
+            a1.detach().numpy(),
+            network.weight.data.squeeze().detach().numpy())
+        np.testing.assert_almost_equal(b1.item(), network.bias.data.item())
+
+        # Now change the learning rate. With a large initial learning rate,
+        # gradient descent will overshoot. It then uses line search to find
+        # an appropriate step size.
+        reset_network()
+        lr = 0.5
+        loss_minimal_decrement = 1e-4
+        step_size_reduction = 0.2
+        dut2 = line_search_gd.LineSearchGD(
+            network.parameters(), lr=lr, momentum=0, min_step_size=1e-8,
+            loss_minimal_decrement=loss_minimal_decrement,
+            step_size_reduction=step_size_reduction)
+        a2 = a0 - grad0[:2] * lr
+        b2 = b0 - grad0[2] * lr
+        loss2 = loss_fun(a2, b2)
+        # Make sure using step size = 0.5 would overshoot.
+        assert(loss2 > loss0)
+        step_size = 0.5
+        a, b = a2, b2
+        while loss_fun(a, b)[0] > loss0[0] + \
+                step_size * loss_minimal_decrement * (-grad0 @ grad0):
+            a = a0 - grad0[:2] * step_size
+            b = b0 - grad0[2] * step_size
+            step_size *= step_size_reduction
+        loss2 = dut2.step(closure, loss0)
+
+        np.testing.assert_allclose(
+            network.weight.data.squeeze().detach().numpy(), a.detach().numpy())
+        np.testing.assert_almost_equal(network.bias.data.item(), b.item())
+        np.testing.assert_almost_equal(
+            loss2[0].item(), loss_fun(a, b)[0].item())
+
+        # Change boththe learning rate and the minimal step size. Now the line
+        # search stops even though the Armijo's condition is not satisfied.
+        reset_network()
+        lr = 0.5
+        loss_minimal_decrement = 1e-4
+        step_size_reduction = 0.2
+        min_step_size = step_size / step_size_reduction * 1.1
+
+        dut3 = line_search_gd.LineSearchGD(
+            network.parameters(), lr=lr, momentum=0,
+            min_step_size=min_step_size,
+            loss_minimal_decrement=loss_minimal_decrement,
+            step_size_reduction=step_size_reduction)
+        loss3 = dut3.step(closure, loss0)
+        step_size = lr
+        while step_size > min_step_size:
+            a = a0 - grad0[:2] * step_size
+            b = b0 - grad0[2] * step_size
+            step_size *= step_size_reduction
+        np.testing.assert_allclose(
+            network.weight.data.squeeze().detach().numpy(), a.detach().numpy())
+        np.testing.assert_almost_equal(network.bias.data.item(), b.item())
+        np.testing.assert_almost_equal(
+            loss3[0].item(), loss_fun(a, b)[0].item())
+        self.assertGreater(
+            loss3[0].item(), loss0[0].item() +
+            (loss_minimal_decrement * step_size/step_size_reduction *
+             -grad0 @ grad0).item())
+
+        reset_network()
+        lr = 0.5
+        loss_minimal_decrement = 1e-4
+        step_size_reduction = 0.2
+        min_step_size = 1e-6
+
+        dut4 = line_search_gd.LineSearchGD(
+            network.parameters(), lr=lr, momentum=0,
+            min_step_size=min_step_size,
+            loss_minimal_decrement=loss_minimal_decrement,
+            step_size_reduction=step_size_reduction)
+        loss = closure()
+        while loss[0] > 1e-6:
+            dut4.zero_grad()
+            loss[0].backward()
+            loss = dut4.step(closure, loss)
+
+        np.testing.assert_allclose(
+            network(x).squeeze().detach().numpy(), np.zeros(3), atol=1e-3)
+
+
+if __name__ == "__main__":
+    unittest.main()
