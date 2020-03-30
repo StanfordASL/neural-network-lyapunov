0ff71094fe570b1ad506fce0f0ae00a538671f01
blandry <ben.acc.landry@gmail.com>
2020-03-30 12:04:34 -0700

    changing double pendulum value parameters

--- robust_value_approx/test/double_pendulum_utils.py
+++ robust_value_approx/test/double_pendulum_utils.py
@@ -66,13 +66,13 @@ class DoublePendulum(nonlinear_system.NonlinearSystem):
 
 def get_value_function(N):
     sys = DoublePendulum(torch.float64)
-    dt_lo = .2
-    dt_up = .2
+    dt_lo = .1
+    dt_up = .1
     vf = value_nlp.NLPValueFunction(
         sys.x_lo, sys.x_up, sys.u_lo, sys.u_up, dt_lo, dt_up)
     vf.add_segment(N-1, sys.dyn, lambda x: sys.dyn(x, arraylib=jax.numpy))
     Q = np.diag([.1, .1, .1, .1])
-    R = np.diag([.01, .01])
+    R = np.diag([.001, .001])
     x_desired = np.array([np.pi, 0., 0., 0.])
     cost_exp = 0
     for n in range(vf.N-1):
@@ -84,7 +84,7 @@ def get_value_function(N):
             arraylib=jax.numpy)
         vf.add_step_cost(n, fun, fun_jax)
     Qt = np.diag([.1, .1, .1, .1]) * vf.N**cost_exp
-    Rt = np.diag([.01, .01])
+    Rt = np.diag([.001, .001])
     fun = lambda x: sys.quad_cost(
         x, Q=Qt, R=Rt, x_desired=x_desired, arraylib=np)
     fun_jax = lambda x: sys.quad_cost(
