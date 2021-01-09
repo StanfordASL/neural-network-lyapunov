c958be8bc23f75a2d1cd6bf007ad80c38eba75fd
Hongkai Dai <daihongkai@gmail.com>
2021-01-09 12:03:25 -0800

    Add the most recent adversarial samples to the training set. (#246)

--- neural_network_lyapunov/test/test_train_lyapunov.py
+++ neural_network_lyapunov/test/test_train_lyapunov.py
@@ -165,17 +165,17 @@ class TestTrainLyapunovReLU(unittest.TestCase):
         loss_expected += dut.lyapunov_positivity_sample_cost_weight *\
             lyapunov_hybrid_system.lyapunov_positivity_loss_at_samples(
                 relu_at_equilibrium, x_equilibrium,
-                state_samples_all[-dut.max_sample_pool_size:], V_lambda,
-                dut.lyapunov_positivity_epsilon,
+                positivity_state_samples_new[-dut.max_sample_pool_size:],
+                V_lambda, dut.lyapunov_positivity_epsilon,
                 R=R_options.R(), margin=dut.lyapunov_positivity_sample_margin)
         loss_expected += dut.lyapunov_derivative_sample_cost_weight *\
             lyapunov_hybrid_system.\
             lyapunov_derivative_loss_at_samples_and_next_states(
                 V_lambda, dut.lyapunov_derivative_epsilon,
-                state_samples_all[-dut.max_sample_pool_size:],
-                state_samples_next[-dut.max_sample_pool_size:], x_equilibrium,
-                dut.lyapunov_derivative_eps_type, R=R_options.R(),
-                margin=dut.lyapunov_derivative_sample_margin)
+                derivative_state_samples_new[-dut.max_sample_pool_size:],
+                derivative_state_samples_next_new[-dut.max_sample_pool_size:],
+                x_equilibrium, dut.lyapunov_derivative_eps_type,
+                R=R_options.R(), margin=dut.lyapunov_derivative_sample_margin)
         lyapunov_positivity_mip_return = lyapunov_hybrid_system.\
             lyapunov_positivity_as_milp(
                 x_equilibrium, V_lambda, dut.lyapunov_positivity_epsilon,
