ad0a1eb9a70ddb2581f4513f9cfc24e4b8b8d34a
Hongkai Dai <daihongkai@gmail.com>
2021-01-22 10:36:15 -0800

    Generate unicycle training data by doing trajectory optimization (#259)

--- neural_network_lyapunov/examples/car/unicycle.py
+++ neural_network_lyapunov/examples/car/unicycle.py
@@ -74,8 +74,8 @@ class UnicycleReLUModel:
     perceptron with (leaky) ReLU units.
     We consider the origin being the equilibrium state of the unicycle. The
     neural network predicts the next state as the following
-    [delta_pos_x, delta_pos_y] = ϕ(θ, vel, θ_dot) - ϕ(θ, 0, θ_dot)
-    where vel is the velocity along the car heading direction.
+    [delta_pos_x, delta_pos_y] = ϕ(θ, vel, θ_dot) - ϕ(0, 0, 0)
+    where vel is the velocity along the unicycle heading direction.
     """
     def __init__(self, dtype, x_lo: torch.Tensor, x_up: torch.Tensor,
                  u_lo: torch.Tensor, u_up: torch.Tensor,
@@ -138,6 +138,162 @@ class UnicycleReLUModel:
         θ[n+1] = θ[n] + θ̇_dot[n]*dt
         when thetadot_as_input is true
         [pos_x[n+1], pos_y[n+1]] =
+        [pos_x[n], pos_y[n]] + ϕ(θ[n], vel[n], θ_dot[n]) − ϕ(0, 0, 0)
+        else
+        [pos_x[n+1], pos_y[n+1]] =
+        [pos_x[n], pos_y[n]] + ϕ(θ[n], vel[n]) − ϕ(0, 0)
+        """
+        assert (isinstance(x_start, torch.Tensor))
+        assert (isinstance(u_start, torch.Tensor))
+        if len(x_start.shape) == 1:
+            # single data point.
+            assert (torch.all(u_start <= self.u_up))
+            assert (torch.all(u_start >= self.u_lo))
+            if self.thetadot_as_input:
+                network_input = torch.stack(
+                    (x_start[2], u_start[0], u_start[1]))
+                network_input_zero = torch.zeros((3, ), dtype=self.dtype)
+            else:
+                network_input = torch.stack((x_start[2], u_start[0]))
+                network_input_zero = torch.zeros((2, ), dtype=self.dtype)
+            delta_position = self.dynamics_relu(
+                network_input) - self.dynamics_relu(network_input_zero)
+            position_next = x_start[:2] + delta_position
+            theta_next = x_start[2] + u_start[1] * self.dt
+            return torch.cat((position_next, theta_next.reshape((-1, ))))
+        elif len(x_start.shape) == 2:
+            # batch of data
+            if self.thetadot_as_input:
+                network_input = torch.cat((x_start[:, 2].reshape(
+                    (-1, 1)), u_start),
+                                          dim=1)
+                network_input_zero = torch.zeros((3, ), dtype=self.dtype)
+            else:
+                network_input = torch.cat((x_start[:, 2].reshape(
+                    (-1, 1)), u_start[:, 0].reshape((-1, 1))),
+                                          dim=1)
+                network_input_zero = torch.zeros((2, ), dtype=self.dtype)
+            delta_position = self.dynamics_relu(
+                network_input) - self.dynamics_relu(network_input_zero)
+            position_next = x_start[:, :2] + delta_position
+            theta_next = (x_start[:, 2] + u_start[:, 1] * self.dt).reshape(
+                (-1, 1))
+            return torch.cat((position_next, theta_next), dim=1)
+
+    def possible_dx(self, x, u):
+        """
+        TODO(hongkai.dai): I will deprecate this function soon. But for now I
+        keep it for backward maintanence as other systems in relu_system.py
+        """
+        assert (isinstance(x, torch.Tensor))
+        assert (isinstance(u, torch.Tensor))
+        return [self.step_forward(x, u)]
+
+    def add_dynamics_constraint(self, mip: gurobi_torch_mip.GurobiTorchMIP,
+                                x_var, x_next_var, u_var, slack_var_name,
+                                binary_var_name):
+        """
+        Add the dynamic constraints a mixed-integer linear constraints. Refer
+        to relu_system.py for the common API.
+        The constraints are
+        θ[n+1] = θ[n] + θ̇_dot[n]*dt
+        when thetadot_as_input is true
+        [pos_x[n+1], pos_y[n+1]] =
+        [pos_x[n], pos_y[n]] + ϕ(θ[n], vel[n], θ_dot[n]) − ϕ(0, 0, 0)
+        else
+        [pos_x[n+1], pos_y[n+1]] =
+        [pos_x[n], pos_y[n]] + ϕ(θ[n], vel[n]) − ϕ(0, 0)
+        """
+        assert (isinstance(mip, gurobi_torch_mip.GurobiTorchMIP))
+        if self.thetadot_as_input:
+            network_input_lo = torch.stack(
+                (self.x_lo[2], self.u_lo[0], self.u_lo[1]))
+            network_input_up = torch.stack(
+                (self.x_up[2], self.u_up[0], self.u_up[1]))
+            network_input_size = 3
+        else:
+            network_input_lo = torch.stack((self.x_lo[2], self.u_lo[0]))
+            network_input_up = torch.stack((self.x_up[2], self.u_up[0]))
+            network_input_size = 2
+        mip_cnstr_result, _, _, _, _ = \
+            self.dynamics_relu_free_pattern.output_constraint(
+                network_input_lo, network_input_up,
+                method=mip_utils.PropagateBoundsMethod.LP)
+        # First add mip_cnstr_result. But don't impose the constraint on the
+        # output of the network (we will impose the output constraint
+        # afterwards inside this function)
+        if self.thetadot_as_input:
+            input_vars = [x_var[2], u_var[0], u_var[1]]
+        else:
+            input_vars = [x_var[2], u_var[0]]
+        forward_slack, forward_binary = \
+            mip.add_mixed_integer_linear_constraints(
+                mip_cnstr_result, input_vars, None, slack_var_name,
+                binary_var_name, "unicycle_forward_dynamics_ineq",
+                "unicycle_forward_dyamics_eq", None)
+        # Now add the constraint on the output of the network, that
+        # [delta_pos_x, delta_pos_y] =
+        # ϕ(θ[n], vel[n], θ_dot[n])−ϕ(0, 0, 0)
+        # Namely [x_next[0] - x[0], x_next[1] - x[1]] = Aout_slack * s + Cout
+        #   −ϕ(0, 0, 0)
+        assert (mip_cnstr_result.Aout_input is None)
+        assert (mip_cnstr_result.Aout_binary is None)
+        mip.addMConstrs([
+            torch.eye(2, dtype=self.dtype), -torch.eye(2, dtype=self.dtype),
+            -mip_cnstr_result.Aout_slack
+        ], [x_next_var[:2], x_var[:2], forward_slack],
+                        b=mip_cnstr_result.Cout - self.dynamics_relu(
+                            torch.zeros(
+                                (network_input_size, ), dtype=self.dtype)),
+                        sense=gurobipy.GRB.EQUAL,
+                        name="unicycle_forward_dynamics_output")
+
+        # Now add the constraint θ[n+1] = θ[n] + θ_dot[n] * dt
+        mip.addLConstr([torch.tensor([1., -1., -self.dt], dtype=self.dtype)],
+                       [[x_next_var[2], x_var[2], u_var[1]]],
+                       sense=gurobipy.GRB.EQUAL,
+                       rhs=0.,
+                       name="unicycle_theta_dynamics")
+        return forward_slack, forward_binary
+
+
+class UnicycleReLUZeroVelModel(UnicycleReLUModel):
+    """
+    We model the discrete-time dynamics of a unicycle using a multi-layer
+    perceptron with (leaky) ReLU units.
+    We consider the origin being the equilibrium state of the unicycle. The
+    neural network predicts the next state as the following
+    [delta_pos_x, delta_pos_y] = ϕ(θ, vel, θ_dot) - ϕ(θ, 0, θ_dot)
+    where vel is the velocity along the unicycle heading direction. Note that
+    when vel=0, this model enforces that the position doesn't change.
+    """
+    def __init__(self, dtype, x_lo: torch.Tensor, x_up: torch.Tensor,
+                 u_lo: torch.Tensor, u_up: torch.Tensor,
+                 dynamics_relu: torch.nn.Sequential, dt: float,
+                 thetadot_as_input: bool):
+        """
+        @param x_lo The lower bound of the state.
+        @param x_up The upper bound of the state.
+        @param u_lo The lower input limits.
+        @param u_up The upper input limits.
+        @param dynamics_relu ϕ in the documentation above.
+        @param dt The delta time (which is also used in training the dynamics
+        relu).
+        @param thetadot_as_input A boolean flag, if set to True, then the
+        network input is [θ, vel, θ_dot]; otherwise the network input is
+        [θ, vel]
+        """
+        super(UnicycleReLUZeroVelModel,
+              self).__init__(dtype, x_lo, x_up, u_lo, u_up, dynamics_relu, dt,
+                             thetadot_as_input)
+
+    def step_forward(self, x_start: torch.Tensor, u_start: torch.Tensor) ->\
+            torch.Tensor:
+        """
+        Compute x[n+1] according to
+        θ[n+1] = θ[n] + θ̇_dot[n]*dt
+        when thetadot_as_input is true
+        [pos_x[n+1], pos_y[n+1]] =
         [pos_x[n], pos_y[n]] + ϕ(θ[n], vel[n], θ_dot[n]) − ϕ(θ[n], 0, θ_dot[n])
         else
         [pos_x[n+1], pos_y[n+1]] =
@@ -190,15 +346,6 @@ class UnicycleReLUModel:
             theta_next = (x_start[:, 2] + u_start[:, 1] * self.dt).reshape(
                 (-1, 1))
             return torch.cat((position_next, theta_next), dim=1)
-
-    def possible_dx(self, x, u):
-        """
-        TODO(hongkai.dai): I will deprecate this function soon. But for now I
-        keep it for backward maintanence as other systems in relu_system.py
-        """
-        assert (isinstance(x, torch.Tensor))
-        assert (isinstance(u, torch.Tensor))
-        return [self.step_forward(x, u)]
 
     def add_dynamics_constraint(self, mip: gurobi_torch_mip.GurobiTorchMIP,
                                 x_var, x_next_var, u_var, slack_var_name,
