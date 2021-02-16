3cab3444a721f4e4497079e4d5b6d8cb5ab7273b
Lujie Yang <yanglujie@berkeley.edu>
2021-02-15 20:10:11 -0500

    Fix duplicate code, calculate duplicate r for each angle

--- neural_network_lyapunov/examples/car/value_iteration.py
+++ neural_network_lyapunov/examples/car/value_iteration.py
@@ -6,7 +6,7 @@ import math
 import argparse
 
 
-def value_iteration(nx, r, epsilon=0.0001, discount_factor=0.95):
+def value_iteration(nx, r, u_optimal, epsilon=0.0001, discount_factor=0.95):
     """
     Value Iteration Algorithm.
     Args:
@@ -15,16 +15,21 @@ def value_iteration(nx, r, epsilon=0.0001, discount_factor=0.95):
         epsilon: threshold for convergence.
         discount_factor: Gamma discount factor.
     Returns:
+        policy: numpy array of size (nx, nu). policy[i] is the optimal control
+        of state s[i].
         V: numpy array of size nx. V[i] is the optimal value function of
         state s[i].
     """
 
     def one_step_lookahead(V, r, i):
-        return np.min(r[i, :] + discount_factor * V)
+        j = np.argmin(r[i, :] + discount_factor * V)
+        v = r[i, j] + discount_factor * V[j]
+        return j, v
 
     # start with inital value function and intial policy
 
     V = np.zeros(nx)
+    policy = np.zeros((nx, 2))
 
     n = 0
     # while not the optimal policy
@@ -36,7 +41,7 @@ def value_iteration(nx, r, epsilon=0.0001, discount_factor=0.95):
         # loop over state space
         for i in range(nx):
 
-            v = one_step_lookahead(V, r, i)
+            j, v = one_step_lookahead(V, r, i)
 
             # get the biggest difference between best action value and our old
             # value function
@@ -45,13 +50,14 @@ def value_iteration(nx, r, epsilon=0.0001, discount_factor=0.95):
 
             # apply bellman optimality eqn
             V[i] = v
+            policy[i, :] = u_optimal[i, j, :]
 
         # if optimal value function
         if (delta < epsilon):
             break
         n += 1
 
-    return V
+    return V, policy
 
 
 def calculate_r_complete(
@@ -68,7 +74,7 @@ def calculate_r_complete(
     # r = np.ones((ns, ns))*1000
     r = np.full((ns, ns), np.inf)
     u_optimal = np.full((ns, ns, 2), np.nan)
-    for i in range(10):
+    for i in range(ns):
         i0, i1, i2 = flat_to_3d(i)
         if i % 100 == 0:
             print("r iteration: ", i)
@@ -107,23 +113,23 @@ def calculate_r_duplicate(
     u,
         dt):
     r = np.full((ns, ns), np.inf)
-    u_optimal = np.zeros((ns, ns))
+    u_optimal = np.full((ns, ns, 2), np.nan)
     # Calculate a quarter of a duplicated block at the four corners
-    i0s = [0, 0, n_grid_xy - 1, n_grid_xy - 1]
-    i1s = [0, n_grid_xy - 1, 0, n_grid_xy - 1]
+    i01s = [[0, 0], [0, n_grid_xy - 1],
+            [n_grid_xy - 1, 0], [n_grid_xy - 1, n_grid_xy - 1]]
     for i2 in range(n_grid_angle):
+        print("i2 iteration: ", i2)
         # Construct duplicated 3D block
         r3d = np.full((2 * n0 + 1, 2 * n1 + 1, 2 * n2 + 1), np.inf)
-        u3d = np.zeros((2 * n0 + 1, 2 * n1 + 1, 2 * n2 + 1))
-        for k in range(len(i0s)):
-            i0 = i0s[k]
-            i1 = i1s[k]
+        u3d = np.full((2 * n0 + 1, 2 * n1 + 1, 2 * n2 + 1, 2), np.nan)
+        for i01 in i01s:
+            i0 = i01[0]
+            i1 = i01[1]
             i = threeD_to_flat(i0, i1, i2)
 
             for j in range(ns):
                 j0, j1, j2 = flat_to_3d(j)
-                if j % 1000 == 0:
-                    print("Duplicate iteration: ", j)
+
                 if i != j and (i0 - n0) <= j0 <= (i0 + n0) and \
                         (i1 - n1) <= j1 <= (i1 + n1) and \
                         (i2 - n2) <= j2 <= (i2 + n2):
@@ -135,29 +141,35 @@ def calculate_r_duplicate(
                         x, np.linspace(s[:, i], s[:, j], nT).T)
                     prog.SetInitialGuess(dt, dt_max * np.ones((nT - 1,)))
                     result = mp.Solve(prog)
+
                     if result.is_success():
                         optimal_cost = result.get_optimal_cost()
                         r3d[j0 - i0 + n0, j1 - i1 + n1,
                             j2 - i2 + n2] = optimal_cost
                         r[i, j] = optimal_cost
-                        optimal_control = result.GetSolution(u)[0]
+
+                        optimal_control = result.GetSolution(u)[:, 0]
                         u3d[j0 - i0 + n0, j1 - i1 + n1,
                             j2 - i2 + n2] = optimal_control
                         u_optimal[i, j] = optimal_control
 
-    for i in range(ns):
-        i0, i1, i2 = flat_to_3d(i)
-        if i % 100 == 0:
-            print("r iteration: ", i)
-        for j in range(ns):
-            j0, j1, j2 = flat_to_3d(j)
-            if i != j and 0 <= j0 - i0 + \
-                    n0 < r3d.shape[0] and \
-                    0 <= j1 - i1 + n1 < r3d.shape[1] and \
-                    0 <= j2 - i2 + n2 < r3d.shape[2]:
-                r[i, j] = r3d[j0 - i0 + n0, j1 - i1 + n1, j2 - i2 + n2]
+        for i0 in range(n_grid_xy):
+            for i1 in range(n_grid_xy):
+                if [i0, i1] not in i01:
+                    i = threeD_to_flat(i0, i1, i2)
+                    for j in range(ns):
+                        j0, j1, j2 = flat_to_3d(j)
+                        if i != j and 0 <= j0 - i0 + \
+                                n0 < r3d.shape[0] and \
+                                0 <= j1 - i1 + n1 < r3d.shape[1] and \
+                                0 <= j2 - i2 + n2 < r3d.shape[2]:
+                            r[i, j] = r3d[j0 - i0 + n0, j1 - i1 + n1,
+                                          j2 - i2 + n2]
+                            u_optimal[i, j] = u3d[j0 - i0 +
+                                                  n0, j1 - i1 + n1,
+                                                  j2 - i2 + n2]
 
-    return r
+    return r, u_optimal
 
 
 def flat_to_3d(i):
@@ -195,8 +207,8 @@ if __name__ == "__main__":
     prog, initial_val_constraint, final_val_constraint, x, u, dt =\
         unicycle_traj_opt.construct_traj_opt(
             nT, u_lo, u_up, dt_min, dt_max)
-    n_grid_xy = 10
-    n_grid_angle = 30
+    n_grid_xy = 20
+    n_grid_angle = 60
     s = np.array(np.meshgrid(np.linspace(x_lo[0], x_up[0], n_grid_xy),
                              np.linspace(x_lo[1], x_up[1], n_grid_xy),
                              np.linspace(x_lo[2], x_up[2], n_grid_angle)))
@@ -204,12 +216,14 @@ if __name__ == "__main__":
     s = s.reshape((3, n_grid_xy * n_grid_xy * n_grid_angle))
     ns = s.shape[1]
     n0 = math.ceil(u_up[0] * dt_max * (nT - 1) /
-                   (x_up[0] - x_lo[0]) * n_grid_xy)
+                   (x_up[0] - x_lo[0]) * (n_grid_xy - 1))
     n1 = math.ceil(u_up[0] * dt_max * (nT - 1) /
-                   (x_up[1] - x_lo[1]) * n_grid_xy)
+                   (x_up[1] - x_lo[1]) * (n_grid_xy - 1))
     n2 = math.ceil(u_up[1] * dt_max * (nT - 1) /
-                   (x_up[2] - x_lo[2]) * n_grid_angle)
+                   (x_up[2] - x_lo[2]) * (n_grid_angle - 1))
     if args.load_r:
+        # r = np.random.random((ns, ns))
+        # u_optimal = np.random.random((ns, ns, 2))
         r = np.load(
             "neural_network_lyapunov/examples/car/value/r_" +
             str(nT) +
@@ -232,12 +246,12 @@ if __name__ == "__main__":
             str(dt_max) +
             ".npy")
     else:
-        r, u_optimal = calculate_r_complete(
-            ns, n0, n1, n2, prog, initial_val_constraint,
-            final_val_constraint, x, u, dt)
-        r = calculate_r_duplicate(ns, n0, n1, n2,
-                                  prog, initial_val_constraint,
-                                  final_val_constraint, x, u, dt)
+        # r, u_optimal = calculate_r_complete(
+        #     ns, n0, n1, n2, prog, initial_val_constraint,
+        #     final_val_constraint, x, u, dt)
+        r, u_optimal = calculate_r_duplicate(ns, n0, n1, n2,
+                                             prog, initial_val_constraint,
+                                             final_val_constraint, x, u, dt)
         # r(0,0) = 0
         eq_ind = np.argmin(np.linalg.norm(s, axis=0))
         r[eq_ind, eq_ind] = 0
@@ -262,7 +276,7 @@ if __name__ == "__main__":
             str(dt_max),
             u_optimal)
 
-    V = value_iteration(ns, r, discount_factor=1)
+    V, policy = value_iteration(ns, r, u_optimal, discount_factor=1)
     np.save(
         "neural_network_lyapunov/examples/car/value/V_" +
         str(nT) +
@@ -273,6 +287,16 @@ if __name__ == "__main__":
         "_" +
         str(dt_max),
         V)
+    np.save(
+        "neural_network_lyapunov/examples/car/value/policy_" +
+        str(nT) +
+        "_" +
+        str(n_grid_xy) +
+        "_" +
+        str(n_grid_angle) +
+        "_" +
+        str(dt_max),
+        policy)
     np.save(
         "neural_network_lyapunov/examples/car/value/s_" +
         str(nT) +
