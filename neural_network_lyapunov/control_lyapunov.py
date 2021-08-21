3a7c239770a742c3c6d94858bac71e6791b27271
Hongkai Dai <daihongkai@gmail.com>
2021-08-21 15:54:44 -0700

    add lyapunov_derivative_as_milp for control lyapunov (#3)
    Currently implement the case with V_lambda = 0 and epsilon = 0.

--- neural_network_lyapunov/control_lyapunov.py
+++ neural_network_lyapunov/control_lyapunov.py
@@ -2,6 +2,7 @@ import neural_network_lyapunov.lyapunov as lyapunov
 import neural_network_lyapunov.utils as utils
 import neural_network_lyapunov.control_affine_system as control_affine_system
 import neural_network_lyapunov.gurobi_torch_mip as gurobi_torch_mip
+import neural_network_lyapunov.mip_utils as mip_utils
 import torch
 import gurobipy
 
@@ -15,6 +16,24 @@ class ControlAffineSystemConstraintReturn(lyapunov.SystemConstraintReturn):
               self).__init__(slack, binary)
         self.mip_cnstr_f = None
         self.mip_cnstr_G = None
+
+
+class LyapDerivMilpReturn:
+    def __init__(self, milp, x, relu_beta, dphidx_times_G_l1_binary,
+                 system_constraint_return):
+        """
+        Args:
+          relu_beta: The binary variable indicating the activeness of each ReLU
+          unit in the network.
+          dphidx_times_G_l1_binary: When computing
+          |∂ϕ/∂x*G(x) * (u_up - u_up)/2|₁, we may need to introduce binary
+          variables for the l1 norm.
+        """
+        self.milp = milp
+        self.x = x
+        self.relu_beta = relu_beta
+        self.dphidx_times_G_l1_binary = dphidx_times_G_l1_binary
+        self.system_constraint_return = system_constraint_return
 
 
 class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
@@ -52,16 +71,34 @@ class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
                            control_affine_system.ControlPiecewiseAffineSystem))
         super(ControlLyapunov, self).__init__(system, lyapunov_relu)
 
-    def lyapunov_derivative(self, x, x_equilibrium, V_lambda, epsilon, *, R):
+    def lyapunov_derivative(self,
+                            x,
+                            x_equilibrium,
+                            V_lambda,
+                            epsilon,
+                            *,
+                            R,
+                            subgradient_rule: str = "max"):
         """
         Compute minᵤ V̇ + ε*V
         subject to u_lo <= u <= u_up
         Note that ∂V/∂x = ∂ϕ/∂x + λ ∑ᵢ sign(R[i, :](x−x*))R[i, :]
+
+        Note that V is not differentiable everywhere. When V is not
+        differentiable at x, we choose its sub-gradient according to
+        subgradient_rule.
+
         Args:
           x_equilibrium: x* in the documentation.
           V_lambda: λ in the documentation.
           epsilon: ε in the documentation.
           R: A full column rank matrix. Use the identity matrix if R=None.
+          subgradient_rule: Can be either "max", "min", or "all".
+            If "max", then we compute the maximal of V̇ among all possible
+            subgradients.
+            If "min", then we compute the minimal of V̇ among all possible
+            subgradients.
+            If "all", then we compute all V̇ for every possible subgradients.
         """
         assert (isinstance(x, torch.Tensor))
         assert (x.shape == (self.system.x_dim, ))
@@ -91,14 +128,19 @@ class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
                 p=1,
                 dim=1)
         V = self.lyapunov_value(x, x_equilibrium, V_lambda, R=R)
-        return torch.min(Vdot.squeeze()) + epsilon * V
+        if subgradient_rule == "max":
+            return torch.max(Vdot.squeeze()) + epsilon * V
+        elif subgradient_rule == "min":
+            return torch.min(Vdot.squeeze()) + epsilon * V
+        elif subgradient_rule == "all":
+            return Vdot.squeeze() + epsilon * V
 
     def add_system_constraint(
         self,
         milp: gurobi_torch_mip.GurobiTorchMIP,
         x: list,
         f: list,
-        G: list,
+        Gt: list,
         *,
         binary_var_type=gurobipy.GRB.BINARY
     ) -> ControlAffineSystemConstraintReturn:
@@ -106,10 +148,11 @@ class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
         Add the (mixed-integer linear) constraints of f(x) and G(x).
 
         Args:
-          G: G is a 2D list. len(G) = u_dim. len(G[i]) = x_dim
+          Gt: Gt is a 2D list. len(G) = u_dim. len(G[i]) = x_dim. Gt is the
+          transpose of system.G(x).
         """
         assert (len(f) == self.system.x_dim)
-        assert (len(G) == self.system.u_dim)
+        assert (len(Gt) == self.system.u_dim)
         # Add constraint that x_lo <= x <= x_up
         milp.addMConstrs(
             [torch.eye(self.system.x_dim, dtype=self.system.dtype)], [x],
@@ -133,7 +176,7 @@ class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
             "f_output", binary_var_type)
         for i in range(self.system.u_dim):
             slack_Gi, binary_Gi = milp.add_mixed_integer_linear_constraints(
-                mip_cnstr_G[i], x, G[i], f"slack_G[{i}]", f"binary_G[{i}]",
+                mip_cnstr_G[i], x, Gt[i], f"slack_G[{i}]", f"binary_G[{i}]",
                 "G[{i}]_ineq", "G[{i}]_eq", "G[{i}]_out", binary_var_type)
             slack.extend(slack_Gi)
             binary.extend(binary_Gi)
@@ -141,3 +184,193 @@ class ControlLyapunov(lyapunov.LyapunovHybridLinearSystem):
         ret.mip_cnstr_f = mip_cnstr_f
         ret.mip_cnstr_G = mip_cnstr_G
         return ret
+
+    def lyapunov_derivative_as_milp(
+            self,
+            x_equilibrium,
+            V_lambda,
+            epsilon,
+            eps_type: lyapunov.ConvergenceEps,
+            *,
+            R,
+            lyapunov_lower=None,
+            lyapunov_upper=None,
+            x_warmstart=None,
+            binary_var_type=gurobipy.GRB.BINARY) -> LyapDerivMilpReturn:
+        """
+        Formulate maxₓ f(x)
+                  s.t lyapunov_lower <= V(x) <= lyapunov_upper
+                      x_lo <= x <= x_up
+        as a mixed-integer linear program.
+        The objective f(x) takes different forms depending on @p eps_type.
+        If eps_type = ConvergenceEps.kExpLower:
+          f(x) =  V̇ + εV(x)
+        if eps_type = ConvergenceEps.kExpUpper:
+          f(x) =  -V̇ - εV(x)
+        if eps_type = ConvergenceEps.kAsymp:
+          f(x) = V̇ + ε|R(x−x*)|₁
+        The lyapunov function is defined as V(x) = ϕ(x) − ϕ(x*) + λ|R(x−x*)|₁
+
+        Args:
+          x_equilibrium: The equilibrium state x*
+          V_lambda: λ in the Lyapunov function.
+          epsilon: ε in the documentation above.
+          eps_type: Refer to ConvergenceEps for more details.
+          R: A full column-rank matrix.
+          lyapunov_lower: The lower bound of the Lyapunov function.
+          lyapunov_upper: The upper bound of the Lyapunov function.
+          x_warmstart: Warm-start value for x in the optimization.
+          binary_var_type: whether to register the binary variable as
+          continuous relaxation or not.
+
+        Returns:
+          (milp, x, beta, gamma, s, z)
+        """
+        assert (isinstance(x_equilibrium, torch.Tensor))
+        assert (x_equilibrium.shape == (self.system.x_dim, ))
+        if lyapunov_lower is not None:
+            assert (isinstance(lyapunov_lower, float))
+        if lyapunov_upper is not None:
+            assert (isinstance(lyapunov_upper, float))
+        assert (isinstance(V_lambda, float))
+        assert (isinstance(epsilon, float))
+        assert (isinstance(eps_type, lyapunov.ConvergenceEps))
+        R = lyapunov._get_R(R, self.system.x_dim, x_equilibrium.device)
+
+        # First add the system dynamics constraint f(x), G(x).
+        milp = gurobi_torch_mip.GurobiTorchMILP(self.system.dtype)
+        x = milp.addVars(self.system.x_dim,
+                         lb=-gurobipy.GRB.INFINITY,
+                         vtype=gurobipy.GRB.CONTINUOUS,
+                         name="x")
+        f = milp.addVars(self.system.x_dim,
+                         lb=-gurobipy.GRB.INFINITY,
+                         vtype=gurobipy.GRB.CONTINUOUS,
+                         name="f")
+        # Notice that torch.tensor(Gt) is the transpose of system.G(x)
+        Gt = [None] * self.system.u_dim
+        for i in range(self.system.u_dim):
+            Gt[i] = milp.addVars(self.system.x_dim,
+                                 lb=-gurobipy.GRB.INFINITY,
+                                 vtype=gurobipy.GRB.CONTINUOUS,
+                                 name=f"Gt[{i}]")
+
+        system_constraint_return = self.add_system_constraint(
+            milp, x, f, Gt, binary_var_type=binary_var_type)
+
+        # We need to compute ∂ϕ/∂x*f(x) and ∂ϕ/∂x*G(x), which requires the
+        # binary variables indicating the activeness of each ReLU unit.
+        relu_slack, relu_beta, a_relu_out, b_relu_out, _ = \
+            self.add_lyap_relu_output_constraint(milp, x)
+
+        mip_cnstr_dphidx_times_G = [None] * self.system.u_dim
+        if self.network_bound_propagate_method == \
+                mip_utils.PropagateBoundsMethod.IA:
+            f_lo, f_up = self.system.compute_f_range_ia()
+            G_lo, G_up = self.system.compute_G_range_ia()
+            mip_cnstr_dphidx_times_f = \
+                self.lyapunov_relu_free_pattern.output_gradient_times_vector(
+                    f_lo, f_up)
+            for i in range(self.system.u_dim):
+                mip_cnstr_dphidx_times_G[i] = self.lyapunov_relu_free_pattern.\
+                    output_gradient_times_vector(G_lo[i], G_up[i])
+        else:
+            raise NotImplementedError
+
+        dphidx_times_f_slack, _ = milp.add_mixed_integer_linear_constraints(
+            mip_cnstr_dphidx_times_f, f, None, "f_slack", relu_beta, "f_ineq",
+            "f_eq", "f", binary_var_type)
+        # I expect these entries to be None, so that Vdot_coeff/Vdot_vars
+        # won't include these entries.
+        assert (mip_cnstr_dphidx_times_f.Aout_input is None)
+        assert (mip_cnstr_dphidx_times_f.Aout_binary is None)
+        assert (mip_cnstr_dphidx_times_f.Cout is None)
+        # V̇ will be Vdot_coeff * Vdot_vars + Vdot_constant. We will use V̇ in
+        # the objective.
+        Vdot_coeff = [mip_cnstr_dphidx_times_f.Aout_slack.reshape((-1, ))]
+        Vdot_vars = [dphidx_times_f_slack]
+        Vdot_constant = torch.tensor(0, dtype=self.system.dtype)
+        # An alternative formulation is not to introduce the variable
+        # dphidx_times_G, but write it as an affine function of the slack
+        # variable in mip_cnstr_dphidx_times_G.
+        dphidx_times_G = milp.addVars(self.system.u_dim,
+                                      lb=-gurobipy.GRB.INFINITY,
+                                      name="dphidx_times_G")
+        for i in range(self.system.u_dim):
+            milp.add_mixed_integer_linear_constraints(
+                mip_cnstr_dphidx_times_G[i], Gt[i], [dphidx_times_G[i]],
+                f"G[{i}]_slack", relu_beta, f"G[{i}]_ineq", f"G[{i}]_eq",
+                f"G[{i}]", binary_var_type)
+        u_mid = (self.system.u_lo + self.system.u_up) / 2
+        # Add ∂ϕ/∂x*G(x) * (u_lo + u_up)/2 to Vdot
+        Vdot_coeff.append(u_mid.reshape((-1, )))
+        Vdot_vars.append(dphidx_times_G)
+
+        # Add -|∂ϕ/∂x*G(x) * (u_up - u_up)/2|₁ to Vdot
+        dphidx_times_G_l1_binary = [None] * self.system.u_dim
+        for i in range(self.system.u_dim):
+            if mip_cnstr_dphidx_times_G[i].Wz_lo[-1] >= 0:
+                # ∂ϕ/∂x*Gᵢ(x) >= 0. The absolute value is itself.
+                Vdot_coeff.append(
+                    -(self.system.u_up[i] - self.system.u_lo[i]).reshape(
+                        (-1, )) / 2)
+                Vdot_vars.append([dphidx_times_G[i]])
+            elif mip_cnstr_dphidx_times_G[i].Wz_up[-1] <= 0:
+                # ∂ϕ/∂x*Gᵢ(x) <= 0. The absolute value is its negation.
+                Vdot_coeff.append(
+                    (self.system.u_up[i] - self.system.u_lo[i]).reshape(
+                        (-1, )) / 2)
+                Vdot_vars.append([dphidx_times_G[i]])
+            else:
+                Ain_dphidx_times_G, Ain_dphidx_times_G_l1_slack,\
+                    Ain_dphidx_times_G_l1_binary, rhs_in_dphidx_times_G_l1 = \
+                    utils.replace_absolute_value_with_mixed_integer_constraint(
+                        mip_cnstr_dphidx_times_G[i].Wz_lo[-1].squeeze(),
+                        mip_cnstr_dphidx_times_G[i].Wz_up[-1].squeeze(),
+                        self.system.dtype)
+                dphidx_times_G_l1_slack = milp.addVars(
+                    1,
+                    lb=-gurobipy.GRB.INFINITY,
+                    name=f"dphidx_times_G[{i}]_l1_slack")
+                dphidx_times_G_l1_binary[i] = milp.addVars(
+                    1,
+                    vtype=gurobipy.GRB.BINARY,
+                    name=f"dphidx_times_G[{i}]_l1_binary")[0]
+                milp.addMConstrs([
+                    Ain_dphidx_times_G.reshape((-1, 1)),
+                    Ain_dphidx_times_G_l1_slack.reshape((-1, 1)),
+                    Ain_dphidx_times_G_l1_binary.reshape((-1, 1))
+                ], [[dphidx_times_G[i]], dphidx_times_G_l1_slack,
+                    [dphidx_times_G_l1_binary[i]]],
+                                 sense=gurobipy.GRB.LESS_EQUAL,
+                                 b=rhs_in_dphidx_times_G_l1,
+                                 name=f"dphidx_times_G[{i}]_l1")
+                Vdot_coeff.append(
+                    -0.5 * (self.system.u_up[i] - self.system.u_lo[i]).reshape(
+                        (-1, )))
+                Vdot_vars.append(dphidx_times_G_l1_slack)
+
+        # TODO(hongkai.dai): implement V_lambda != 0 case.
+        if V_lambda != 0:
+            raise NotImplementedError
+        # We need to compute |R(x−x*)|₁
+        # l1_slack, l1_binary = self.add_state_error_l1_constraint(
+        #    milp,
+        #    x_equilibrium,
+        #    x,
+        #    R=R,
+        #    slack_name="l1_slack",
+        #    binary_var_name="l1_binary",
+        #    binary_var_type=binary_var_type)
+        # TODO(hongkai.dai): implement epsilon != 0 case.
+        if epsilon != 0:
+            raise NotImplementedError
+
+        milp.setObjective(Vdot_coeff,
+                          Vdot_vars,
+                          Vdot_constant,
+                          sense=gurobipy.GRB.MAXIMIZE)
+
+        return LyapDerivMilpReturn(milp, x, relu_beta,
+                                   dphidx_times_G_l1_binary,
+                                   system_constraint_return)
--- neural_network_lyapunov/test/test_control_lyapunov.py
+++ neural_network_lyapunov/test/test_control_lyapunov.py
@@ -8,6 +8,7 @@ import gurobipy
 import neural_network_lyapunov.control_affine_system as control_affine_system
 import neural_network_lyapunov.utils as utils
 import neural_network_lyapunov.gurobi_torch_mip as gurobi_torch_mip
+import neural_network_lyapunov.lyapunov as lyapunov
 
 
 class TestControlLyapunov(unittest.TestCase):
@@ -42,17 +43,23 @@ class TestControlLyapunov(unittest.TestCase):
         self.lyapunov_relu1[4].bias.data = torch.tensor([2], dtype=self.dtype)
 
     def lyapunov_derivative_tester(self, dut, x, x_equilibrium, V_lambda,
-                                   epsilon, R):
+                                   epsilon, R, subgradient_rule):
         vdot = dut.lyapunov_derivative(x,
                                        x_equilibrium,
                                        V_lambda,
                                        epsilon,
-                                       R=R)
+                                       R=R,
+                                       subgradient_rule=subgradient_rule)
 
         dphi_dx = utils.relu_network_gradient(dut.lyapunov_relu, x).squeeze(1)
         dl1_dx = V_lambda * utils.l1_gradient(R @ (x - x_equilibrium)) @ R
         v = dut.lyapunov_value(x, x_equilibrium, V_lambda, R=R)
-        vdot_expected = np.inf
+        if subgradient_rule == "max":
+            vdot_expected = -np.inf
+        elif subgradient_rule == "min":
+            vdot_expected = np.inf
+        elif subgradient_rule == "all":
+            subgradient_rule = []
         for i in range(dphi_dx.shape[0]):
             for j in range(dl1_dx.shape[0]):
                 dV_dx = dphi_dx[i] + dl1_dx[j]
@@ -62,9 +69,26 @@ class TestControlLyapunov(unittest.TestCase):
                     vdot_expected_ij += torch.minimum(
                         dV_dx_times_G[k] * dut.system.u_lo[k],
                         dV_dx_times_G[k] * dut.system.u_up[k])
-                if vdot_expected_ij < vdot_expected:
+                if subgradient_rule == "max" and vdot_expected_ij > \
+                        vdot_expected:
                     vdot_expected = vdot_expected_ij
-        self.assertAlmostEqual(vdot.item(), vdot_expected.item())
+                elif subgradient_rule == "min" and vdot_expected_ij < \
+                        vdot_expected:
+                    vdot_expected = vdot_expected_ij
+                elif subgradient_rule == "all":
+                    vdot_expected.append(vdot_expected_ij)
+        if subgradient_rule in ("min", "max"):
+            self.assertAlmostEqual(vdot[0].item(), vdot_expected.item())
+        elif subgradient_rule == "all":
+            # We don't care the order in vdot.
+            self.assertEqual(vdot.shape[0] == len(vdot_expected))
+            for i in range(vdot.shape[0]):
+                found_match = False
+                for j in len(vdot_expected):
+                    if np.abs(vdot[i].item() - vdot_expected[j]) < 1E-10:
+                        found_match = True
+                        break
+                self.assertTrue(found_match)
 
     def test_lyapunov_derivative1(self):
         # Test with linear system.
@@ -75,26 +99,27 @@ class TestControlLyapunov(unittest.TestCase):
         epsilon = 0.1
 
         x = torch.tensor([0.5, 1.5], dtype=self.dtype)
-        self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
-                                        epsilon, R)
-        # Some ReLU inputs equal to 0.
-        x = torch.tensor([1, 2], dtype=self.dtype)
-        self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
-                                        epsilon, R)
-        # Some l1-norm entry equals to 0.
-        x = torch.tensor([0, 1], dtype=self.dtype)
-        self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
-                                        epsilon, R)
+        for subgradient_rule in ("min", "max", "all"):
+            self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
+                                            epsilon, R, subgradient_rule)
+            # Some ReLU inputs equal to 0.
+            x = torch.tensor([1, 2], dtype=self.dtype)
+            self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
+                                            epsilon, R, subgradient_rule)
+            # Some l1-norm entry equals to 0.
+            x = torch.tensor([0, 1], dtype=self.dtype)
+            self.lyapunov_derivative_tester(dut, x, x_equilibrium, V_lambda,
+                                            epsilon, R, subgradient_rule)
 
     def add_system_constraint_tester(self, dut, x_val: torch.Tensor,
                                      is_feasible):
         milp = gurobi_torch_mip.GurobiTorchMIP(self.dtype)
         x = milp.addVars(dut.system.x_dim, lb=x_val, ub=x_val)
         f = milp.addVars(dut.system.x_dim, lb=-gurobipy.GRB.INFINITY)
-        G = [None] * dut.system.u_dim
+        Gt = [None] * dut.system.u_dim
         for i in range(dut.system.u_dim):
-            G[i] = milp.addVars(dut.system.x_dim, lb=-gurobipy.GRB.INFINITY)
-        ret = dut.add_system_constraint(milp, x, f, G)
+            Gt[i] = milp.addVars(dut.system.x_dim, lb=-gurobipy.GRB.INFINITY)
+        ret = dut.add_system_constraint(milp, x, f, Gt)
         # Linear system doesn't introduce slack/binary variables.
         self.assertEqual(len(ret.slack), 0)
         self.assertEqual(len(ret.binary), 0)
@@ -105,10 +130,10 @@ class TestControlLyapunov(unittest.TestCase):
             self.assertEqual(milp.gurobi_model.status,
                              gurobipy.GRB.Status.OPTIMAL)
             f_val = np.array([v.x for v in f])
-            G_val = [None] * dut.system.u_dim
+            Gt_val = [None] * dut.system.u_dim
             for i in range(dut.system.u_dim):
-                G_val[i] = [v.x for v in G[i]]
-            G_val = np.array(G_val).T
+                Gt_val[i] = [v.x for v in Gt[i]]
+            G_val = np.array(Gt_val).T
 
             f_expected = dut.system.f(x_val)
             G_expected = dut.system.G(x_val)
@@ -133,6 +158,107 @@ class TestControlLyapunov(unittest.TestCase):
             dut, torch.tensor([-3, 0], dtype=self.dtype), False)
         self.add_system_constraint_tester(
             dut, torch.tensor([0, 4], dtype=self.dtype), False)
+
+    def lyapunov_derivative_as_milp_tester(self, dut, x_equilibrium, V_lambda,
+                                           epsilon, eps_type, R,
+                                           lyapunov_lower, lyapunov_upper,
+                                           binary_var_type, is_feasible):
+        lyap_deriv_return = dut.lyapunov_derivative_as_milp(
+            x_equilibrium,
+            V_lambda,
+            epsilon,
+            eps_type,
+            R=R,
+            lyapunov_lower=lyapunov_lower,
+            lyapunov_upper=lyapunov_upper,
+            x_warmstart=None,
+            binary_var_type=binary_var_type)
+        lyap_deriv_return.milp.gurobi_model.setParam(
+            gurobipy.GRB.Param.OutputFlag, False)
+        lyap_deriv_return.milp.gurobi_model.setParam(
+            gurobipy.GRB.Param.DualReductions, False)
+        lyap_deriv_return.milp.gurobi_model.optimize()
+        if is_feasible:
+            self.assertEqual(lyap_deriv_return.milp.gurobi_model.status,
+                             gurobipy.GRB.Status.OPTIMAL)
+            # First evaluate the optimal solution.
+            x_sol = torch.tensor([v.x for v in lyap_deriv_return.x],
+                                 dtype=self.dtype)
+            Vdot_sol = dut.lyapunov_derivative(x_sol,
+                                               x_equilibrium,
+                                               V_lambda,
+                                               0.,
+                                               R=R,
+                                               subgradient_rule="max")
+            V_sol = dut.lyapunov_value(x_sol, x_equilibrium, V_lambda, R=R)
+            if eps_type == lyapunov.ConvergenceEps.ExpLower:
+                np.testing.assert_allclose(
+                    (Vdot_sol + epsilon * V_sol).item(),
+                    lyap_deriv_return.milp.gurobi_model.ObjVal)
+            else:
+                raise NotImplementedError
+
+            # Sample many x_val within the bounds. make sure the objective
+            # value is smaller than the optimal one.
+            torch.manual_seed(0)
+            x_samples = utils.uniform_sample_in_box(dut.system.x_lo,
+                                                    dut.system.x_up, 1000)
+            V_samples = dut.lyapunov_value(x_samples,
+                                           x_equilibrium,
+                                           V_lambda,
+                                           R=R)
+            acceptable_samples = torch.tensor([True] * x_samples.shape[0])
+            if lyapunov_lower is not None:
+                acceptable_samples = V_samples >= lyapunov_lower
+            if lyapunov_upper is not None:
+                acceptable_samples = torch.logical_and(
+                    acceptable_samples, V_samples <= lyapunov_upper)
+            x_samples = x_samples[acceptable_samples]
+            V_samples = V_samples[acceptable_samples]
+            for i in range(x_samples.shape[0]):
+                if eps_type in (lyapunov.ConvergenceEps.ExpLower,
+                                lyapunov.ConvergenceEps.Asymp):
+                    Vdot_sample = dut.lyapunov_derivative(
+                        x_samples[i],
+                        x_equilibrium,
+                        V_lambda,
+                        0.,
+                        R=R,
+                        subgradient_rule="max")
+                    if eps_type == lyapunov.ConvergenceEps.ExpLower:
+                        self.assertLessEqual(
+                            (Vdot_sample + epsilon * V_samples[i]).item(),
+                            lyap_deriv_return.milp.gurobi_model.ObjVal)
+                    elif eps_type == lyapunov.ConvergenceEps.Asymp:
+                        self.assertLessEqual((
+                            Vdot_sample + epsilon *
+                            torch.norm(R @ (x_samples[i] - x_equilibrium), p=1)
+                        ).item(), lyap_deriv_return.milp.gurobi_model.ObjVal)
+                else:
+                    raise NotImplementedError
+
+        else:
+            self.assertEqual(lyap_deriv_return.milp.gurobi_model.status,
+                             gurobipy.GRB.Status.INFEASIBLE)
+
+    def test_lyapunov_derivative_as_milp1(self):
+        # V_lambda = 0 and epsilon = 0, only compute ϕdot.
+        dut = mut.ControlLyapunov(self.linear_system, self.lyapunov_relu1)
+        x_equilibrium = torch.tensor([0.1, 0.2], dtype=self.dtype)
+        V_lambda = 0.
+        epsilon = 0.
+        R = torch.tensor([[1., 0.], [0.5, 1.], [1., -1.]], dtype=self.dtype)
+        self.lyapunov_derivative_as_milp_tester(
+            dut,
+            x_equilibrium,
+            V_lambda,
+            epsilon,
+            lyapunov.ConvergenceEps.ExpLower,
+            R,
+            None,
+            None,
+            gurobipy.GRB.BINARY,
+            is_feasible=True)
 
 
 if __name__ == "__main__":
