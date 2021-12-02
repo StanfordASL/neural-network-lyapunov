a017bbc1719c714fb4cad493870dc0c1446a8408
hongkai-dai <daihongkai@gmail.com>
2021-12-01 23:15:06 -0800

    Use x/y coordinate of the sphere instead of azimuth and altitude angles.

--- doc/pole_balance_formulation.py
+++ doc/pole_balance_formulation.py
@@ -7,27 +7,28 @@ xA, yA, zA = sympy.symbols("xA yA zA")
 length = sympy.symbols("l")
 me, ms = sympy.symbols("me ms")
 g = sympy.symbols("g")
-alpha = sympy.symbols("alpha")
-beta = sympy.symbols("beta")
 xAdot, yAdot, zAdot = sympy.symbols("xAd yAd zAd")
-alphadot, betadot = sympy.symbols("alphad betad")
 xAddot, yAddot, zAddot = sympy.symbols("xAdd yAdd zAdd")
-alphaddot, betaddot = sympy.symbols("alphadd betadd")
+xAB, yAB, zAB = sympy.symbols("xAB yAB zAB")
+xABdot, yABdot = sympy.symbols("xABd yABd")
+xABddot, yABddot = sympy.symbols("xABdd yABdd")
 p_WS = sympy.Matrix([
-    xA + length * sympy.cos(beta) * sympy.cos(alpha),
-    yA + length * sympy.cos(beta) * sympy.sin(alpha),
-    zA + length * sympy.sin(beta)
+    xA + xAB, yA + yAB, zA + sympy.sqrt(length**2 - xAB**2 - yAB**2)
 ])
-q = [xA, yA, zA, alpha, beta]
-qdot = [xAdot, yAdot, zAdot, alphadot, betadot]
-qddot = [xAddot, yAddot, zAddot, alphaddot, betaddot]
+q = [xA, yA, zA, xAB, yAB]
+qdot = [xAdot, yAdot, zAdot, xABdot, yABdot]
+qddot = [xAddot, yAddot, zAddot, xABddot, yABddot]
 v_WS = sympy.Matrix([
     sum([sympy.diff(p_WS[i], var) * vardot for (var, vardot) in zip(q, qdot)])
     for i in range(3)
 ])
+for i in range(3):
+    print(f"v_WS[{i}]")
+    print(v_WS[i])
+
 T = 0.5 * me * (xAdot**2 + yAdot**2 +
                 zAdot**2) + 0.5 * ms * (v_WS[0]**2 + v_WS[1]**2 + v_WS[2]**2)
-U = me * g * zA + ms * g * (zA + length * sympy.sin(beta))
+U = me * g * zA + ms * g * p_WS[2]
 L = T - U
 dLdqdot = [sympy.diff(L, qdot[i]) for i in range(5)]
 dLdq = [sympy.diff(L, q[i]) for i in range(5)]
@@ -36,11 +37,11 @@ lhs = [
     sum([sympy.diff(dLdqdot[row], qdot[i]) * qddot[i]
          for i in range(5)]) - dLdq[row] for row in range(5)
 ]
-for row in range(3):
+for row in range(2):
     print(f"row {row}:")
     print(sympy.simplify(lhs[row]))
-
-print("simplify row 3:")
-print(sympy.simplify(lhs[3] / (ms * length)))
-print("simplify row 4:")
-print(sympy.simplify(lhs[4] / (ms * length)))
+for row in range(2, 5):
+    print(f"row {row}:")
+    print(
+        sympy.simplify(lhs[row].subs(sympy.sqrt(length**2 - xAB**2 - yAB**2),
+                                     zAB)))
