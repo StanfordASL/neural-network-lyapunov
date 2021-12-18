Fix a bug computing Wz bounds for lyapunov derivative. (#418)
Adds relu_binary to the programto compute the Wz bounds.

neural_network_lyapunov/continuous_time_lyapunov.py
@@ -96,8 +96,9 @@ class LyapunovContinuousTimeSystem(lyapunov.LyapunovHybridLinearSystem):
         system_constraint_return = self.add_system_constraint(
             milp, x, xdot, binary_var_type=binary_var_type)
         # Adds the mixed-integer constraints to compute V(x).
-        relu_slack, relu_binary, relu_a_out, relu_b_out, _ = \
-            self.add_lyap_relu_output_constraint(milp, x)
+        relu_slack, relu_binary, relu_a_out, relu_b_out,\
+            relu_output_mip_cnstr_ret = self.add_lyap_relu_output_constraint(
+                milp, x)
 
         l1_slack, l1_binary = self.add_state_error_l1_constraint(
             milp,
@@ -134,11 +135,19 @@ class LyapunovContinuousTimeSystem(lyapunov.LyapunovHybridLinearSystem):
         elif self.network_bound_propagate_method in (
                 mip_utils.PropagateBoundsMethod.LP,
                 mip_utils.PropagateBoundsMethod.MIP):
+            x_next_bound_prog_binary_type = gurobipy.GRB.CONTINUOUS if\
+                self.network_bound_propagate_method == \
+                mip_utils.PropagateBoundsMethod.LP else gurobipy.GRB.BINARY
+            _, x_next_bound_relu_binary = system_constraint_return.\
+                x_next_bound_prog.add_mixed_integer_linear_constraints(
+                    relu_output_mip_cnstr_ret, system_constraint_return.x_var,
+                    None, "", "", "", "", "", x_next_bound_prog_binary_type)
             relu_z_lo, relu_z_up, relu_Wz_lo, relu_Wz_up, _ = \
                 self.lyapunov_relu_free_pattern.\
                 _compute_Wz_bounds_optimization(
                     system_constraint_return.x_next_bound_prog.gurobi_model,
-                    system_constraint_return.x_next_bound_var, relu_binary)
+                    system_constraint_return.x_next_bound_var,
+                    x_next_bound_relu_binary)
         else:
             raise NotImplementedError
         dphidx_times_xdot_ret = self.lyapunov_relu_free_pattern.\
@@ -195,7 +204,7 @@ class LyapunovContinuousTimeSystem(lyapunov.LyapunovHybridLinearSystem):
                 mip_utils.PropagateBoundsMethod.MIP):
             Rxdot_lb = torch.empty((R.shape[0], ), dtype=self.system.dtype)
             Rxdot_ub = torch.empty((R.shape[0], ), dtype=self.system.dtype)
-            for i in R.shape[0]:
+            for i in range(R.shape[0]):
                 system_constraint_return.x_next_bound_prog.setObjective(
                     [R[i]], [system_constraint_return.x_next_bound_var], 0.,
                     gurobipy.GRB.MAXIMIZE)

neural_network_lyapunov/test/test_continuous_time_lyapunov.py
@@ -16,6 +16,7 @@ import neural_network_lyapunov.relu_system as relu_system
 import neural_network_lyapunov.test.test_hybrid_linear_system as\
     test_hybrid_linear_system
 import neural_network_lyapunov.test.test_lyapunov as test_lyapunov
+import neural_network_lyapunov.mip_utils as mip_utils
 
 
 class TestLyapunovContinuousTimeSystem(unittest.TestCase):
@@ -165,7 +166,8 @@ class TestLyapunovContinuousTimeSystem(unittest.TestCase):
                 self.assertEqual(ret.milp.gurobi_model.status,
                                  gurobipy.GRB.Status.INFEASIBLE)
 
-    def test_lyapunov_derivative_as_milp(self):
+    def test_lyapunov_derivative_as_milp1(self):
+        # Test with IA.
         dut1 = mut.LyapunovContinuousTimeSystem(self.system1,
                                                 self.lyapunov_relu1)
         R = torch.tensor([[1, 3], [-1, 2], [0, 1]], dtype=self.dtype)
@@ -182,6 +184,25 @@ class TestLyapunovContinuousTimeSystem(unittest.TestCase):
             dut1,
             self.system1.x_equilibrium,
             V_lambda=0.2,
+            epsilon=0.5,
+            eps_type=lyapunov.ConvergenceEps.ExpLower,
+            R=R,
+            lyapunov_lower=None,
+            lyapunov_upper=None)
+
+    def test_lyapunov_derivative_as_milp2(self):
+        # Test with network bound propagate = LP.
+        network_bound_propagate_method = mip_utils.PropagateBoundsMethod.LP
+        self.system1.network_bound_propagate_method = \
+            network_bound_propagate_method
+        dut1 = mut.LyapunovContinuousTimeSystem(self.system1,
+                                                self.lyapunov_relu1)
+        dut1.network_bound_propagate_method = network_bound_propagate_method
+        R = torch.tensor([[1, 3], [-1, 2], [0, 1]], dtype=self.dtype)
+        self.lyapunov_derivative_as_milp_tester(
+            dut1,
+            self.system1.x_equilibrium,
+            V_lambda=0.,
             epsilon=0.5,
             eps_type=lyapunov.ConvergenceEps.ExpLower,
             R=R,